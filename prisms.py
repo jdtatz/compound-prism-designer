ef7c04ed06179993ab20db50a6a6d293d1eb4a3f
Jacob Tatz <tatz.j@husky.neu.edu>
2017-05-23 17:36:38 -0400

    Moved stuff around

--- prisms.py
+++ prisms.py
@@ -1,74 +1,9 @@
 import numba as nb
 import numpy as np
 from numpy import pi
-from utils import jit, cfunc, gradient, vec_ptr, minimizer
+from utils import *
 from collections import namedtuple
 
-"""
-Utility Functions
-"""
-
-
-@jit
-def get_poly_coeffs(g, n):
-    """Get the polynomial coefficients and remainder when treating the input as a n-polynomial function"""
-    x = np.linspace(1.0, -1.0, g.size)
-    poly = np.empty((g.size, n + 1))
-    for i in range(n + 1):
-        poly[:, i] = x ** i
-    coeffs = np.linalg.pinv(poly) @ g
-    remainder = np.sum((g - poly @ coeffs) ** 2)
-    return coeffs, remainder
-
-
-@jit
-def nonlinearity(x):
-    """Calculate the nonlinearity of the given delta spectrum"""
-    g = gradient(gradient(x))
-    return np.sqrt(np.sum(g ** 2))
-
-
-@jit
-def nonlinearity_error(x):
-    """Calculate the nonlinearity error of the given delta spectrum"""
-    grad = gradient(x)
-    if np.max(grad) < 0.0 or np.min(grad) < 0.0:
-        grad -= np.min(grad)
-    return np.sum(gradient(grad) ** 2)
-
-
-@jit
-def beam_compression(thetas, nwaves):
-    """Calculate the beam compression of the path taken by the central ray of light"""
-    ts = np.abs(np.cos(thetas))
-    K = np.prod(ts[1::2, nwaves//2]) / np.prod(ts[:-1:2, nwaves//2])
-    return K
-
-
-@jit
-def spectral_sampling_ratio(w, delta_spectrum, sampling_domain_is_wavenumber):
-    """Calculate the SSR of the given delta spectrum"""
-    if sampling_domain_is_wavenumber:
-        w = 1000.0 / w
-    grad = gradient(delta_spectrum) / gradient(w)
-    maxval, minval = np.max(grad), np.min(grad)
-    # If both maxval and minval are negative, then we should swap the ratio to
-    # get SSR > 1.
-    return minval / maxval if abs(maxval) < abs(minval) else maxval / minval
-
-
-@jit
-def transmission(thetas, n, nwaves):
-    """Calculate transmission percentage using Fresnel equations, unfinished"""
-    ns = np.vstack((np.ones((1, nwaves)), n, np.ones((1, nwaves))))[:, nwaves//2]
-    n1 = ns[:-1]
-    n2 = ns[1:]
-    # return 100 - 100*np.abs((n1 - n2) / (n1 + n2))**2
-    ci = np.cos(thetas[:-1:2, nwaves // 2])
-    ct = np.cos(thetas[1::2, nwaves // 2])
-    rs = np.abs((n1*ci-n2*ct)/(n1*ci+n2*ct))**2
-    rp = np.abs((n1*ct-n2*ci)/(n1*ct+n2*ci))**2
-    return 100*np.prod(1 - (rs + rp) / 2)
 
 """
 Prism Code
