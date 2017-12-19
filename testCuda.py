926bbf3f637e392b0460ce92bc4c8b1e9cae4e46
Julia Tatz <tatz.j@northeastern.edu>
2017-12-18 19:03:09 -0500

    Formatting changes and small optimizations

--- testCuda.py
+++ testCuda.py
@@ -9,28 +9,30 @@ import operator
 from collections import namedtuple, OrderedDict
 import time
 from compoundprism.glasscat import read_glasscat, calc_n
+
 os.environ['NUMBAPRO_NVVM'] = '/usr/local/cuda/nvvm/lib64/libnvvm.so'
 os.environ['NUMBAPRO_LIBDEVICE'] = '/usr/local/cuda/nvvm/libdevice/'
 
 count = 4
-count_p1 = count+1
-start = 3.01
-radius = 3
+count_p1 = count + 1
+start = 3.5
+radius = 1.5
 height = 25
 theta0 = 0
 nwaves = 100
 deltaC_target = 0 * math.pi / 180
 deltaT_target = 45 * math.pi / 180
-inital_angles = np.full((count,), math.pi/2, np.float32)
+transmission_minimum = 0.7
+inital_angles = np.full((count,), math.pi / 2, np.float32)
 inital_angles[1::2] *= -1
-base_weights = OrderedDict(tir=50.0, valid=5.0, thin=2.5, deviation=5.0, dispersion=30.0, linearity=10000.0)
+base_weights = OrderedDict(tir=50.0, valid=5.0, thin=2.5, deviation=5.0, dispersion=30.0, linearity=10000.0, transm=5)
 MeritWeights = namedtuple('MeritWeights', base_weights.keys())
 weights = MeritWeights(**base_weights)
 
 
 @nb.cuda.jit(device=True)
 def ilog2(n):
-    return 0 if n < 2 else 1 + ilog2(n >> 1)
+    return int(math.log(n, 2))
 
 
 @nb.cuda.jit(device=True)
@@ -46,10 +48,8 @@ def roundToPow2(num):
 def create_fold_func(func, warpSize=32):
     @nb.cuda.jit(device=True)
     def warpFold(value, foldLen):
-        i = foldLen / 2
-        while i > 0:
-            value = func(value, nb.cuda.shfl.xor(value, i, foldLen))
-            i >>= 2
+        for i in range(1, 1 + ilog2(foldLen)):
+            value = func(value, nb.cuda.shfl.xor(value, foldLen >> i, foldLen))
         return value
 
     @nb.cuda.jit(device=True)
@@ -86,18 +86,19 @@ def create_fold_func(func, warpSize=32):
             faster = (warpCount - 1) > (1 + ilog2(warpCountPow2) + (0 if isPow2(warpCount) else 1))
             allWarpsFull = foldLen % warpSize == 0
             partialBigEnough = foldLen % warpSize >= warpCountPow2
-            
+
             if faster and (allWarpsFull or partialBigEnough or withinFullWarp):
-              value = sharedArr[warpID % warpCount]
-              if not isPow2(warpCount) and warpID < warpCount - warpCountPow2:
-                value = func(value, sharedArr[warpID + warpCountPow2])
-              value = warpFold(value, warpCountPow2)
-              value = nb.cuda.shfl.idx(value, 0)
+                value = sharedArr[warpID % warpCount]
+                if not isPow2(warpCount) and warpID < warpCount - warpCountPow2:
+                    value = func(value, sharedArr[warpID + warpCountPow2])
+                value = warpFold(value, warpCountPow2)
+                value = nb.cuda.shfl.idx(value, 0)
             else:
-              value = sharedArr[0]
-              for i in range(1, warpCount):
-                value = func(value, sharedArr[i])
+                value = sharedArr[0]
+                for i in range(1, warpCount):
+                    value = func(value, sharedArr[i])
             return value
+
     return fold
 
 
@@ -155,31 +156,28 @@ def merit_error(n, angles):
     sideR = sideL = height / math.cos(offAngle)
     invalidity = 0 > path0 or path0 > sideR or 0 > path1 or path1 > sideR
     incident = theta0 + beta
-    crit_angle =  math.asin(n2)
+    crit_angle = math.pi / 2
     crit_violation_count = syncthreads_popc(abs(incident) >= crit_angle)
     if crit_violation_count > 0:
         return weights.tir * crit_violation_count
     refracted = math.asin((1.0 / n[0, tid]) * math.sin(incident))
     ci, cr = math.cos(incident), math.cos(refracted)
-    t_s = 1 - ((n1*ci-n2*cr)/(n1*ci+n2*cr))**2
-    t_p = 1 - ((n1*cr-n2*ci)/(n1*cr+n2*ci))**2
-    T = 1 - 0.5*(((n1*ci-n2*cr)/(n1*ci+n2*cr))**2 + ((n1*cr-n2*ci)/(n1*cr+n2*ci))**2)
+    T = 1 - (((n1 * ci - n2 * cr) / (n1 * ci + n2 * cr)) ** 2 + ((n1 * cr - n2 * ci) / (n1 * cr + n2 * ci)) ** 2) / 2
     for i in range(1, count + 1):
         n1 = n2
         n2 = n[i, tid] if i < count else 1.0
         alpha = angles[i - 1]
         incident = refracted - alpha
-        
+
         crit_angle = math.asin(n2 / n1) if n2 < n1 else (math.pi / 2)
-        crit_violation_count = syncthreads_popc(abs(incident) >= crit_angle*0.999)
+        crit_violation_count = syncthreads_popc(abs(incident) >= crit_angle * 0.999)
         if crit_violation_count > 0:
             return weights.tir * crit_violation_count
 
-
         if i <= mid:
-            offAngle -= angles[i - 1]
+            offAngle -= alpha
         elif i > mid + 1:
-            offAngle += angles[i - 1]
+            offAngle += alpha
         sideR = height / math.cos(offAngle)
         t1 = math.pi / 2 - refracted * math.copysign(1, alpha)
         t2 = math.pi - abs(alpha) - t1
@@ -192,16 +190,14 @@ def merit_error(n, angles):
             path1 = sideR - (sideL - path1) * los
         invalidity |= 0 > path0 or path0 > sideR or 0 > path1 or path1 > sideR
         sideL = sideR
-            
+
         refracted = math.asin((n1 / n2) * math.sin(incident))
         ci, cr = math.cos(incident), math.cos(refracted)
-        t_s *= 1 - ((n1*ci-n2*cr)/(n1*ci+n2*cr))**2
-        t_p *= 1 - ((n1*cr-n2*ci)/(n1*cr+n2*ci))**2
-        T *= 1 - 0.5*(((n1*ci-n2*cr)/(n1*ci+n2*cr))**2 + ((n1*cr-n2*ci)/(n1*cr+n2*ci))**2)
+        T *= 1 - (((n1 * ci - n2 * cr) / (n1 * ci + n2 * cr)) ** 2 + ((n1 * cr - n2 * ci) / (n1 * cr + n2 * ci)) ** 2) / 2
     delta = theta0 - (refracted + offAngle)
     sharedBlock[tid] = delta
     invalid_count = syncthreads_popc(invalidity)
-    T_too_low = syncthreads_popc(T < 0.70)
+    T_too_low = syncthreads_popc(T < transmission_minimum)
 
     minVal, maxVal, deltaC = sharedBlock[0], sharedBlock[0], sharedBlock[nwaves // 2]
     for i in range(nwaves):
@@ -220,7 +216,7 @@ def merit_error(n, angles):
                 + weights.deviation * (deltaC - deltaC_target) ** 2 \
                 + weights.dispersion * (deltaT - deltaT_target) ** 2 \
                 + weights.linearity * NL \
-                + 5*T_too_low / nwaves
+                + weights.transm * T_too_low / nwaves
     return merit_err
 
 
@@ -232,6 +228,7 @@ def minimizer(n, xmin):
     xe = nb.cuda.local.array(count, nb.f4)
     xc = nb.cuda.local.array(count, nb.f4)
     xcc = nb.cuda.local.array(count, nb.f4)
+    cinital_angles = nb.cuda.const.array_like(inital_angles)
 
     rho = 1
     chi = 2
@@ -243,10 +240,10 @@ def minimizer(n, xmin):
     fatol = 1e-4
 
     for i in range(count):
-        sim[0, i] = xmin[i]
+        sim[0, i] = cinital_angles[i]
     for k in range(count):
         for i in range(count):
-            sim[k + 1, i] = xmin[i]
+            sim[k + 1, i] = cinital_angles[i]
         sim[k + 1, k] *= (1 + nonzdelt)
 
     maxiter = count * 200
@@ -256,7 +253,8 @@ def minimizer(n, xmin):
         fsim[k] = merit_error(n, sim[k])
     ncalls = count
 
-    for i in range(count+1):
+    # sort
+    for i in range(count + 1):
         fx = fsim[i]
         for k in range(count):
             xmin[k] = sim[i, k]
@@ -274,7 +272,7 @@ def minimizer(n, xmin):
     while ncalls < maxfun and iterations < maxiter:
         # Tolerence Check
         maxF, maxS = 0, 0
-        for i in range(1, count+1):
+        for i in range(1, count + 1):
             maxF = max(maxF, abs(fsim[0] - fsim[i]))
             for j in range(count):
                 maxS = max(maxS, abs(sim[0, j] - sim[i, j]))
@@ -310,44 +308,44 @@ def minimizer(n, xmin):
             for i in range(count):
                 sim[-1, i] = xr[i]
             fsim[-1] = fxr
-        else:
+        elif fxr < fsim[-1]:
             # Perform contraction
-            if fxr < fsim[-1]:
+            for i in range(count):
+                xbar = 0
+                for s in range(count):
+                    xbar += sim[s, i]
+                xc[i] = (1 + psi * rho) * xbar / count - psi * rho * sim[-1, i]
+            fxc = merit_error(n, xc)
+            ncalls += 1
+            if fxc <= fxr:
                 for i in range(count):
-                    xbar = 0
-                    for s in range(count):
-                        xbar += sim[s, i]
-                    xc[i] = (1 + psi * rho) * xbar / count - psi * rho * sim[-1, i]
-                fxc = merit_error(n, xc)
-                ncalls += 1
-                if fxc <= fxr:
-                    for i in range(count):
-                        sim[-1, i] = xc[i]
-                    fsim[-1] = fxc
-                else:
-                    doshrink = True
+                    sim[-1, i] = xc[i]
+                fsim[-1] = fxc
             else:
-                # Perform an inside contraction
+                doshrink = True
+        else:
+            # Perform an inside contraction
+            for i in range(count):
+                xbar = 0
+                for s in range(count):
+                    xbar += sim[s, i]
+                xcc[i] = (1 - psi) * xbar / count + psi * sim[-1, i]
+            fxcc = merit_error(n, xcc)
+            ncalls += 1
+            if fxcc < fsim[-1]:
                 for i in range(count):
-                    xbar = 0
-                    for s in range(count):
-                        xbar += sim[s, i]
-                    xcc[i] = (1 - psi) * xbar / count + psi * sim[-1, i]
-                fxcc = merit_error(n, xcc)
-                ncalls += 1
-                if fxcc < fsim[-1]:
-                    for i in range(count):
-                        sim[-1, i] = xcc[i]
-                    fsim[-1] = fxcc
-                else:
-                    doshrink = True
-            if doshrink:
-                for j in range(1, count + 1):
-                    for i in range(count):
-                        sim[j, i] = sim[0, i] + sigma * (sim[j, i] - sim[0, i])
-                    fsim[j] = merit_error(n, sim[j])
-                ncalls += count
-        for i in range(count+1):
+                    sim[-1, i] = xcc[i]
+                fsim[-1] = fxcc
+            else:
+                doshrink = True
+        if doshrink:
+            for j in range(1, count + 1):
+                for i in range(count):
+                    sim[j, i] = sim[0, i] + sigma * (sim[j, i] - sim[0, i])
+                fsim[j] = merit_error(n, sim[j])
+            ncalls += count
+        # sort
+        for i in range(count + 1):
             x = fsim[i]
             for k in range(count):
                 xmin[k] = sim[i, k]
@@ -366,31 +364,10 @@ def minimizer(n, xmin):
     return fsim[0]
 
 
-@nb.cuda.jit((nb.f4[:, :], nb.f4[:]), fastmath=False)
-def optimize(ns, out):
-    tid = nb.cuda.threadIdx.x
-    ind = nb.cuda.blockIdx.x
-    tn = ns.shape[0]
-    nb.cuda.syncthreads()
-    n = nb.cuda.shared.array((count, nwaves), nb.f4)
-    for i in range(count):
-        n[i, tid] = ns[(ind // (tn ** i)) % tn, tid]
-    nb.cuda.syncthreads()
-    cinital_angles = nb.cuda.const.array_like(inital_angles)
-    xmin = nb.cuda.local.array(count, nb.f4)
-    for i in range(count):
-        xmin[i] = cinital_angles[i]
-    fx = minimizer(n, xmin)
-    if tid == 0:
-        out[ind * (1 + count)] = fx
-        for i in range(count):
-            out[ind * (1 + count) + i + 1] = xmin[i]
-
 @nb.cuda.jit((nb.i4[:], nb.f4[:, :], nb.f4[:]), fastmath=False)
-def findder(counter, ns, out):
+def optimize(counter, ns, out):
     tid = nb.cuda.threadIdx.x
     bid = nb.cuda.blockIdx.x
-    cinital_angles = nb.cuda.const.array_like(inital_angles)
     xmin = nb.cuda.local.array(count, nb.f4)
     ind = nb.cuda.shared.array(1, nb.i4)
     best = nb.cuda.shared.array(count, nb.f4)
@@ -407,8 +384,6 @@ def findder(counter, ns, out):
             break
         for i in range(count):
             n[i, tid] = ns[(ind[0] // (nglass ** i)) % nglass, tid]
-        for i in range(count): 
-            xmin[i] = cinital_angles[i]
         nb.cuda.syncthreads()
         fx = minimizer(n, xmin)
         if tid == 0 and fx < bestVal:
@@ -417,42 +392,31 @@ def findder(counter, ns, out):
             for i in range(count):
                 best[i] = xmin[i]
     if tid == 0:
-        outi = bid*(count+2)
+        outi = bid * (count + 2)
         out[outi] = bestVal
-        out[outi+1] = bestInd
+        out[outi + 1] = bestInd
         for i in range(count):
-            out[outi+i+2] = best[i]
+            out[outi + i + 2] = best[i]
 
-
-nb.cuda.select_device(1)
 
 w = np.linspace(500, 820, nwaves, dtype=np.float64)
 gcat = read_glasscat('Glasscat/schott_positive_glass_trimmed_oct2015.agf')
-nglass = len(gcat)
-names = list(gcat.keys())
-glasses = np.array([calc_n(gcat[name], w) for name in names], np.float32)
-nb.cuda.profile_start()
-# d_out = nb.cuda.device_array(((1 + count) * nglass ** count), np.float32)
+nglass, names = len(gcat), list(gcat.keys())
+glasses = np.stack(calc_n(gcat[name], w) for name in names).astype(np.float32)
+
 blockCount = 128
-d_out = nb.cuda.device_array(blockCount*(count+2), np.float32)
-print('Starting')
+d_out = nb.cuda.device_array(blockCount * (count + 2), np.float32)
 counter = np.zeros((10,), np.int32)
+print('Starting')
+
+nb.cuda.profile_start()
 t1 = time.time()
-findder[blockCount, nwaves](counter, glasses, d_out)
-# optimize[nglass ** count, nwaves](glasses, d_out)
+optimize[blockCount, nwaves](counter, glasses, d_out)
 output = d_out.copy_to_host()
 dt = time.time() - t1
-"""
-mi, = np.where(output[::(count+1)] == np.min(output[::(count+1)]))
-gs = [names[((mi[0]) // (nglass ** i)) % nglass] for i in range(count)]
-mi = (count+1) * mi[0]
-print(output[mi], *gs, *(output[mi+1:mi+(count+1)] * 180 / np.pi))
-"""
-mi, = np.where(output[::(count+2)] == np.min(output[::(count+2)]))
-mi = (count+2) * mi[0]
-gs = [names[(int(output[mi+1]) // (nglass ** i)) % nglass] for i in range(count)]
-print(output[mi], *gs, *(output[mi+2:mi+(count+2)] * 180 / np.pi))
-
-print(dt, 's')
 nb.cuda.profile_stop()
 
+index = (count + 2) * np.where(output[::(count + 2)] == np.min(output[::(count + 2)]))[0]
+gs = [names[(int(output[index + 1]) // (nglass ** i)) % nglass] for i in range(count)]
+print(output[index], *gs, *(output[index + 2:index + (count + 2)] * 180 / np.pi))
+print(dt, 's')
