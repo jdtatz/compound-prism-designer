15d8eec69b6eb2bc238799fcf4a116999cff3a7f
Julia Tatz <tatz.j@northeastern.edu>
2018-03-23 17:39:04 +0000

    Modularized config

--- simple.py
+++ simple.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 
-def describe(n, angles, weights, start, radius, height, theta0, deltaC_target, deltaT_target, transmission_minimum):
+def describe(n, angles, config, weights):
     count, nwaves = n.shape
     mid = count // 2
     offAngle = np.sum(angles[:mid]) + angles[mid] / 2
     n1 = 1.0
     n2 = n[0]
-    path0 = (start - radius) / np.cos(offAngle)
-    path1 = (start + radius) / np.cos(offAngle)
-    sideL = height / np.cos(offAngle)
-    incident = theta0 + offAngle
+    path0 = (config.start - config.radius) / np.cos(offAngle)
+    path1 = (config.start + config.radius) / np.cos(offAngle)
+    sideL = config.height / np.cos(offAngle)
+    incident = config.theta0 + offAngle
     crit_angle = np.pi / 2
-    crit_violation_count = np.sum(np.abs(incident) >= crit_angle * 0.999)
+    crit_violation_count = np.sum(np.abs(incident) >= crit_angle * config.crit_angle_prop)
     if crit_violation_count > 0:
         return False, weights.tir * crit_violation_count
     refracted = np.arcsin((n1 / n2) * np.sin(incident))
@@ -25,7 +25,7 @@ def describe(n, angles, weights, start, radius, height, theta0, deltaC_target, d
         incident = refracted - alpha
         
         crit_angle = np.arcsin(np.minimum(n2 / n1, 1))
-        crit_violation_count = np.sum(np.abs(incident) >= crit_angle * 0.999)
+        crit_violation_count = np.sum(np.abs(incident) >= crit_angle * config.crit_angle_prop)
         if crit_violation_count > 0:
             return False, weights.tir * crit_violation_count
 
@@ -33,7 +33,7 @@ def describe(n, angles, weights, start, radius, height, theta0, deltaC_target, d
             offAngle -= alpha
         elif i > mid + 1:
             offAngle += alpha
-        sideR = height / np.cos(offAngle)
+        sideR = config.height / np.cos(offAngle)
         t1 = np.pi / 2 - refracted * np.copysign(1, alpha)
         t2 = np.pi - np.abs(alpha) - t1
         los = np.sin(t1) / np.sin(t2)
@@ -51,15 +51,15 @@ def describe(n, angles, weights, start, radius, height, theta0, deltaC_target, d
         refracted = np.arcsin((n1 / n2) * np.sin(incident))
         ci, cr = np.cos(incident), np.cos(refracted)
         T *= 1 - (((n1 * ci - n2 * cr) / (n1 * ci + n2 * cr)) ** 2 + ((n1 * cr - n2 * ci) / (n1 * cr + n2 * ci)) ** 2) / 2
-    delta_spectrum = theta0 - (refracted + offAngle)
+    delta_spectrum = config.theta0 - (refracted + offAngle)
     transm_spectrum = T
     deltaC = delta_spectrum[nwaves // 2]
     deltaT = (delta_spectrum.max() - delta_spectrum.min())
     meanT = np.sum(transm_spectrum) / nwaves
-    transm_err = np.max(transmission_minimum - meanT, 0)
+    transm_err = np.max(config.transmission_minimum - meanT, 0)
     NL = np.sqrt(np.sum(np.gradient(np.gradient(delta_spectrum)) ** 2))
-    merit_err = weights.deviation * (deltaC - deltaC_target) ** 2 \
-                + weights.dispersion * (deltaT - deltaT_target) ** 2 \
+    merit_err = weights.deviation * (deltaC - config.deltaC_target) ** 2 \
+                + weights.dispersion * (deltaT - config.deltaT_target) ** 2 \
                 + weights.linearity * NL \
                 + weights.transm * transm_err
     return True, merit_err, NL, deltaT, deltaC, delta_spectrum, transm_spectrum
