8cb4dc12a7071dee4aaa018980ae92635b9b0825
Julia Tatz <tatz.j@northeastern.edu>
2018-08-08 16:42:16 -0400

    Figure creation v1

--- simple.py
+++ simple.py
@@ -1,4 +1,5 @@
 import numpy as np
+from svgwrite import Drawing
 
 
 def describe(n, angles, config):
@@ -6,8 +7,17 @@ def describe(n, angles, config):
     count, nwaves = n.shape
     n1 = 1
     n2 = n[0]
+    pts = np.zeros((count+2, 2), dtype=np.float32)
+    pts[::2, 1] = config.height
+    pts[1, 0] = config.height * np.tan(angles[0])
     path0 = (config.start - config.radius) / np.cos(angles[0])
     path1 = (config.start + config.radius) / np.cos(angles[0])
+    ptsL = np.empty((nwaves, count + 1, 2), dtype=np.float32)
+    ptsU = np.empty((nwaves, count + 1, 2), dtype=np.float32)
+    ptsL[:, 0, 0] = pts[1, 0] - (config.start - config.radius) * np.tan(angles[0])
+    ptsL[:, 0, 1] = config.start - config.radius
+    ptsU[:, 0, 0] = pts[1, 0] - (config.start + config.radius) * np.tan(angles[0])
+    ptsU[:, 0, 1] = config.start + config.radius
     sideL = config.height / np.cos(angles[0])
     incident = config.theta0 + angles[0]
     offAngle = angles[1]
@@ -39,14 +49,22 @@ def describe(n, angles, config):
         if alpha > 0:
             path0 *= los
             path1 *= los
+            ptsL[:, i, 1] = np.cos(offAngle) * path0
+            ptsU[:, i, 1] = np.cos(offAngle) * path1
         else:
             path0 = sideR - (sideL - path0) * los
             path1 = sideR - (sideL - path1) * los
+            ptsL[:, i, 1] = config.height - np.cos(offAngle) * (sideR - path0)
+            ptsU[:, i, 1] = config.height - np.cos(offAngle) * (sideR - path1)
+        ptsL[:, i, 0] = pts[i, 0] + np.sin(abs(offAngle)) * path0
+        ptsU[:, i, 0] = pts[i, 0] + np.sin(abs(offAngle)) * path1
+
         invalid_count = np.sum(np.logical_or(np.logical_or(0 > path0, path0 > sideR), np.logical_or(0 > path1, path1 > sideR)))
         if invalid_count > 0:
             return i
         size += np.sqrt(sideL**2 + sideR**2 - 2*sideL*sideR*np.cos(alpha))
         sideL = sideR
+        pts[i+1, 0] = size
 
         refracted = np.arcsin((n1 / n2) * np.sin(incident))
         ci, cr = np.cos(incident), np.cos(refracted)
@@ -63,4 +81,13 @@ def describe(n, angles, config):
                 + config.weight_linearity * NL \
                 + config.weight_transmission * transmission_err \
                 + config.weight_thinness * max(size - config.max_size, 0)
+    triangles = np.stack((pts[:-2], pts[1:-1], pts[2:]), axis=1)
+
+    dwg = Drawing('figure.svg')
+    for i, tri in enumerate(triangles):
+        dwg.add(dwg.polygon(tri.tolist(), fill=('gray' if i%2 else 'white')))
+    dwg.add(dwg.polyline(ptsU[-1].tolist(), stroke='red', stroke_width=0.1, fill='none'))
+    dwg.save()
+
     return merit_err, NL, deltaT, deltaC, size, delta_spectrum, transmission_spectrum
+
