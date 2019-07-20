585892334157a6a93ecbb18979075ac71fd1a991
Julia Tatz <tatz.j@husky.neu.edu>
2019-07-19 21:55:24 -0400

    Added deviation objective & code clean up

--- optimizer.py
+++ optimizer.py
@@ -4,14 +4,16 @@ import prism
 from prism import PrismError, create_catalog, Prism, PmtArray, GaussianBeam
 
 Config = namedtuple("Config", "prism_count, wmin, wmax, waves, prism_height, prism_width, spec_length, beam_width, spec_min_ci, bounds")
-Params = namedtuple("Params", "glasses, thetas, curvature, y_mean, spec_angle")
+Params = namedtuple("Params", "glass_names, glasses, thetas, curvature, y_mean, spec_angle")
 Soln = namedtuple("Soln", "params, objectives")
+nobjective = 3
 
 
 def to_params(config: Config, catalog, p: np.ndarray):
-    gvalues = list(catalog.values())
-    glasses = tuple(gvalues[min(int(i), len(catalog) - 1)] for i in p[:config.prism_count])
-    return Params(glasses, np.asarray(p[config.prism_count:config.prism_count * 2 + 1]), *p[-3:])
+    gvalues = list(catalog.keys())
+    glass_names = [gvalues[min(int(i), len(catalog) - 1)] for i in p[:config.prism_count]]
+    glasses = [catalog[n] for n in glass_names]
+    return Params(glass_names, glasses, np.asarray(p[config.prism_count:config.prism_count * 2 + 1]), *p[-3:])
 
 
 def transmission_data(config: Config, params: Params):
@@ -114,44 +116,58 @@ def fitness(config: Config, params: Params):
             ),
         )
     except PrismError:
-        return [1e6 * config.prism_height] * 2
+        return [1e6 * config.prism_height] * nobjective
 
 
 def show_interactive(config: Config, solns: [Soln]):
     import matplotlib as mpl
     import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
     fig = plt.figure()
     ax = fig.add_subplot(131)
 
-    x, y = np.array([soln.objectives for soln in solns]).T
+    x, y, c = np.array([soln.objectives for soln in solns]).T
     y = -y
+    c = np.rad2deg(np.arcsin(c))
 
-    sc = ax.scatter(x, y, picker=True)
+    sc = ax.scatter(x, y, c=c, cmap="viridis", picker=True)
+    clb = fig.colorbar(sc)
+    clb.ax.set_ylabel("deviation (deg)")
     ax.set_xlabel("size")
-    ax.set_ylabel("transinformation")
+    ax.set_ylabel("mutual information (bits)")
+    # ax.set_zlabel("deviation (deg)")
     """
     x, y, z, c = res.T
     c = 1 - c
 
     sc = ax.scatter(x, y, z, c=c, cmap="viridis", picker=True)
-    ax.set_xlabel("size")
-    ax.set_ylabel("nonlinearity")
-    ax.set_zlabel("spot size")
     clb = fig.colorbar(sc)
     clb.ax.set_ylabel('transmittance')
     """
 
-    px = fig.add_subplot(132)
+    px = fig.add_subplot(232)
+    text_ax = fig.add_subplot(235)
     px.axis('off')
+    text_ax.axis('off')
     gx = fig.add_subplot(233)
     tx = fig.add_subplot(236)
 
     def pick(event):
         soln = sorted((solns[i] for i in event.ind), key=lambda s: s.objectives[-1])[0]
         params = soln.params
-        size, ninfo = soln.objectives
-        print(params)
-        print(f"size: {size:.2}, info: {-ninfo:.2}")
+        size, ninfo, dev = soln.objectives
+        display = f"""Prism ({', '.join(params.glass_names)})
+        angles: {', '.join(f'{np.rad2deg(angle):.4}' for angle in params.thetas)}
+        y_mean: {params.y_mean:.4}
+        curvature: {params.curvature:.4}
+        spectrometer angle: {np.rad2deg(params.spec_angle):.4}
+        objectives: (size={size:.4}, info: {-ninfo:.4}, deviation: {np.rad2deg(np.arcsin(dev)):.4})"""
+        print(display)
+        text_ax.cla()
+        text_ax.text(0, 0.5, display, horizontalalignment='left', verticalalignment='center',
+                     transform=text_ax.transAxes)
+        text_ax.axis('scaled')
+        text_ax.axis('off')
 
         spec_pos, spec_dir = spectrometer_position(config, params)
         spec_end = spec_pos + spec_dir
@@ -229,20 +245,21 @@ class PrismProblem:
         self.config, self.catalog = config, catalog
 
     def fitness(self, v):
-        try:
-            params = to_params(self.config, self.catalog, v)
-            return fitness(self.config, params)
-        except PrismError:
-            return [1e6 * self.config.prism_height] * 2
+        params = to_params(self.config, self.catalog, v)
+        val = fitness(self.config, params)
+        if val[0] > 30 * self.config.prism_height:
+            return [1e6 * self.config.prism_height] * nobjective
+        else:
+            return val
 
     def get_bounds(self):
         nprism = self.config.prism_count
         glass_bounds = nprism * [(0, len(self.catalog) - 1)]
         l, u = np.deg2rad(0), np.deg2rad(90)
         angle_bounds = [(l, u) if i % 2 else (-u, -l) for i in range(nprism + 1)]
-        curvature_bounds = 0.1, 1.0
+        curvature_bounds = 0.01, 1.0
         y_mean_bounds = 0, 1
-        sec = np.deg2rad(45)
+        sec = np.deg2rad(90)
         spec_angle_bounds = -sec, sec
         return tuple(zip(*[
             *glass_bounds,
@@ -254,7 +271,7 @@ class PrismProblem:
 
     @staticmethod
     def get_nobj():
-        return 2
+        return nobjective
 
     @staticmethod
     def get_name():
@@ -262,7 +279,7 @@ class PrismProblem:
 
 
 if __name__ == "__main__":
-    nprism = 2
+    nprism = 3
     nwaves = 100
     nbin = 32
 
@@ -319,8 +336,8 @@ if __name__ == "__main__":
     import pygmo as pg
 
     prob = pg.problem(PrismProblem(config, catalog))
-    algo = pg.algorithm(pg.moead(gen=10000))
-    archi = pg.archipelago(16, algo=algo, prob=prob, pop_size=40)
+    algo = pg.algorithm(pg.nsga2(gen=1000))
+    archi = pg.archipelago(16, algo=algo, prob=prob, pop_size=48)
     archi.evolve()
     archi.wait_check()
     solutions = [Soln(params=to_params(config, catalog, p), objectives=o) for isl in archi for (p, o) in zip(isl.get_population().get_x(), isl.get_population().get_f())]
@@ -329,3 +346,13 @@ if __name__ == "__main__":
 
     show_interactive(config, solutions)
 
+
+'''
+Prism (N-LAF34, N-SF11, P-SK57)
+	angles: -10.9, 76.48, -46.55, 8.042
+	y_mean: 0.507
+	curvature: 0.1015
+	spectrometer angle: 33.76
+	objectives: (size=34.67, info: 4.114, deviation: 1.553)
+
+'''
--- src/glasscat.rs
+++ src/glasscat.rs
@@ -1,5 +1,6 @@
 use alga::general::RealField;
 use core::convert::TryInto;
+use core::str::FromStr;
 use std::collections::BTreeMap;
 
 #[derive(Debug, Display, Clone, Copy)]
@@ -7,8 +8,9 @@ pub enum CatalogErr {
     NameNotFound,
     GlassTypeNotFound,
     InvalidGlassDescription,
+    GlassDescriptionNotFound,
     UnknownGlassType,
-    DuplicateGlass
+    DuplicateGlass,
 }
 
 impl std::error::Error for CatalogErr {}
@@ -17,16 +19,22 @@ impl std::error::Error for CatalogErr {}
 #[derive(Debug, Clone, Copy)]
 pub enum Glass<N> {
     Simple([N; 6]),
-    Sellmeier1([N; 6])
+    Sellmeier1([N; 6]),
 }
 
 impl<N: RealField> Glass<N> {
     pub fn new(form: i32, cd: &[N]) -> Result<Self, CatalogErr> {
+        macro_rules! slice_to_arr {
+            ($n:literal) => {
+                cd.get(..$n)
+                    .and_then(|s| s.try_into().ok())
+                    .ok_or(CatalogErr::InvalidGlassDescription)?
+            };
+        }
         Ok(match form {
-            0 => panic!("No good"),
-            1 => Glass::Simple(cd.get(..6).ok_or(CatalogErr::InvalidGlassDescription)?.try_into().map_err(|_| CatalogErr::InvalidGlassDescription)?),
-            2 => Glass::Sellmeier1(cd.get(..6).ok_or(CatalogErr::InvalidGlassDescription)?.try_into().map_err(|_| CatalogErr::InvalidGlassDescription)?),
-            _ => return Err(CatalogErr::UnknownGlassType)
+            1 => Glass::Simple(slice_to_arr!(6)),
+            2 => Glass::Sellmeier1(slice_to_arr!(6)),
+            _ => return Err(CatalogErr::UnknownGlassType),
         })
     }
 
@@ -42,44 +50,48 @@ impl<N: RealField> Glass<N> {
             }
             Glass::Sellmeier1(cd) => {
                 let w2 = w * w;
-                (N::one() + cd[0] * w2 / (w2 - cd[1]) + cd[2] * w2 / (w2 - cd[3]) + cd[4] * w2 / (w2 - cd[5])).sqrt()
+                (N::one()
+                    + cd[0] * w2 / (w2 - cd[1])
+                    + cd[2] * w2 / (w2 - cd[3])
+                    + cd[4] * w2 / (w2 - cd[5]))
+                    .sqrt()
             }
         }
         /*
-    formula_rhs = 0
-    if dispform == 1:
-        formula_rhs = cd[0] + cd[1] * w ** 2 + cd[2] * w ** -2 + cd[3] * w ** -4 + cd[4] * w ** -6 + cd[5] * w ** -8
-    elif dispform == 2:  # Sellmeier1
-        formula_rhs = 1.0 + cd[0] * w ** 2 / (w ** 2 - cd[1]) + cd[2] * w ** 2 / (w ** 2 - cd[3]) + \
-                      cd[4] * w ** 2 / (w ** 2 - cd[5])
-    elif dispform == 3:  # Herzberger
-        L = 1.0 / (w ** 2 - 0.028)
-        return cd[0] + cd[1] * L + cd[2] * L ** 2 + cd[3] * w ** 2 + cd[4] * w ** 4 + cd[5] * w ** 6
-    elif dispform == 4:  # Sellmeier2
-        formula_rhs = 1.0 + cd[0] + cd[1] * w ** 2 / (w ** 2 - cd[2] ** 2) + cd[3] * w ** 2 / (w ** 2 - cd[4] ** 2)
-    elif dispform == 5:  # Conrady
-        return cd[0] + cd[1] / w + cd[2] / w ** 3.5
-    elif dispform == 6:  # Sellmeier3
-        formula_rhs = 1.0 + cd[0] * w ** 2 / (w ** 2 - cd[1]) + cd[2] * w ** 2 / (w ** 2 - cd[3]) + \
-                      cd[4] * w ** 2 / (w ** 2 - cd[5]) + cd[6] * w ** 2 / (w ** 2 - cd[7])
-    elif dispform == 7:  # HandbookOfOptics1
-        formula_rhs = cd[0] + cd[1] / (w ** 2 - cd[2]) - cd[3] * w ** 2
-    elif dispform == 8:  # HandbookOfOptics2
-        formula_rhs = cd[0] + cd[1] * w ** 2 / (w ** 2 - cd[2]) - cd[3] * w ** 2
-    elif dispform == 9:  # Sellmeier4
-        formula_rhs = cd[0] + cd[1] * w ** 2 / (w ** 2 - cd[2]) + cd[3] * w ** 2 / (w ** 2 - cd[4])
-    elif dispform == 10:  # Extended1
-        formula_rhs = cd[0] + cd[1] * w ** 2 + cd[2] * w ** -2 + cd[3] * w ** -4 + cd[4] * w ** -6 + \
-                      cd[5] * w ** -8 + cd[6] * w ** -10 + cd[7] * w ** -12
-    elif dispform == 11:  # Sellmeier5
-        formula_rhs = 1.0 + cd[0] * w ** 2 / (w ** 2 - cd[1]) + cd[2] * w ** 2 / (w ** 2 - cd[3]) + \
-                      cd[4] * w ** 2 / (w ** 2 - cd[5]) + cd[6] * w ** 2 / (w ** 2 - cd[7]) + \
-                      cd[8] * w ** 2 / (w ** 2 - cd[9])
-    elif dispform == 12:  # Extended2
-        formula_rhs = cd[0] + cd[1] * w ** 2 + cd[2] * w ** -2 + cd[3] * w ** -4 + \
-                      cd[4] * w ** -6 + cd[5] * w ** -8 + cd[6] * w ** 4 + cd[7] * w ** 6
-    return np.sqrt(formula_rhs)
-        */
+        formula_rhs = 0
+        if dispform == 1:
+            formula_rhs = cd[0] + cd[1] * w ** 2 + cd[2] * w ** -2 + cd[3] * w ** -4 + cd[4] * w ** -6 + cd[5] * w ** -8
+        elif dispform == 2:  # Sellmeier1
+            formula_rhs = 1.0 + cd[0] * w ** 2 / (w ** 2 - cd[1]) + cd[2] * w ** 2 / (w ** 2 - cd[3]) + \
+                          cd[4] * w ** 2 / (w ** 2 - cd[5])
+        elif dispform == 3:  # Herzberger
+            L = 1.0 / (w ** 2 - 0.028)
+            return cd[0] + cd[1] * L + cd[2] * L ** 2 + cd[3] * w ** 2 + cd[4] * w ** 4 + cd[5] * w ** 6
+        elif dispform == 4:  # Sellmeier2
+            formula_rhs = 1.0 + cd[0] + cd[1] * w ** 2 / (w ** 2 - cd[2] ** 2) + cd[3] * w ** 2 / (w ** 2 - cd[4] ** 2)
+        elif dispform == 5:  # Conrady
+            return cd[0] + cd[1] / w + cd[2] / w ** 3.5
+        elif dispform == 6:  # Sellmeier3
+            formula_rhs = 1.0 + cd[0] * w ** 2 / (w ** 2 - cd[1]) + cd[2] * w ** 2 / (w ** 2 - cd[3]) + \
+                          cd[4] * w ** 2 / (w ** 2 - cd[5]) + cd[6] * w ** 2 / (w ** 2 - cd[7])
+        elif dispform == 7:  # HandbookOfOptics1
+            formula_rhs = cd[0] + cd[1] / (w ** 2 - cd[2]) - cd[3] * w ** 2
+        elif dispform == 8:  # HandbookOfOptics2
+            formula_rhs = cd[0] + cd[1] * w ** 2 / (w ** 2 - cd[2]) - cd[3] * w ** 2
+        elif dispform == 9:  # Sellmeier4
+            formula_rhs = cd[0] + cd[1] * w ** 2 / (w ** 2 - cd[2]) + cd[3] * w ** 2 / (w ** 2 - cd[4])
+        elif dispform == 10:  # Extended1
+            formula_rhs = cd[0] + cd[1] * w ** 2 + cd[2] * w ** -2 + cd[3] * w ** -4 + cd[4] * w ** -6 + \
+                          cd[5] * w ** -8 + cd[6] * w ** -10 + cd[7] * w ** -12
+        elif dispform == 11:  # Sellmeier5
+            formula_rhs = 1.0 + cd[0] * w ** 2 / (w ** 2 - cd[1]) + cd[2] * w ** 2 / (w ** 2 - cd[3]) + \
+                          cd[4] * w ** 2 / (w ** 2 - cd[5]) + cd[6] * w ** 2 / (w ** 2 - cd[7]) + \
+                          cd[8] * w ** 2 / (w ** 2 - cd[9])
+        elif dispform == 12:  # Extended2
+            formula_rhs = cd[0] + cd[1] * w ** 2 + cd[2] * w ** -2 + cd[3] * w ** -4 + \
+                          cd[4] * w ** -6 + cd[5] * w ** -8 + cd[6] * w ** 4 + cd[7] * w ** 6
+        return np.sqrt(formula_rhs)
+            */
     }
 }
 
@@ -92,25 +104,37 @@ impl<'a, N: RealField> Into<(i32, &'a [N])> for &'a Glass<N> {
     }
 }
 
-pub fn new_catalog<N: RealField + core::str::FromStr>(file: &str) -> Result<BTreeMap<String, Glass<N>>, CatalogErr> {
+pub fn new_catalog<N: RealField + FromStr>(
+    file: &str,
+) -> Result<BTreeMap<String, Glass<N>>, CatalogErr> {
     let mut catalog = BTreeMap::new();
     let mut name = None;
     let mut dispform: i32 = -1;
     for line in file.lines() {
         if line.starts_with("NM") {
             let nm: Vec<_> = line.split(' ').collect();
-            name = Some(nm.get(1).ok_or(CatalogErr::NameNotFound)?.to_uppercase());
-            dispform = nm.get(2).and_then(|d| d.parse().ok()).ok_or(CatalogErr::GlassTypeNotFound)?;
+            if let Some(_) = name.replace(nm.get(1).ok_or(CatalogErr::NameNotFound)?.to_uppercase())
+            {
+                return Err(CatalogErr::GlassDescriptionNotFound);
+            }
+            dispform = nm
+                .get(2)
+                .and_then(|d| d.parse().ok())
+                .ok_or(CatalogErr::GlassTypeNotFound)?;
         } else if line.starts_with("CD") {
-            let cd = line.get(2..)
-                .map(|l|
-                    l.split(' ').filter_map(|s| s.parse().ok()).collect::<Vec<_>>())
+            let cd = line
+                .get(2..)
+                .map(|l| {
+                    l.split(' ')
+                        .filter_map(|s| s.parse().ok())
+                        .collect::<Vec<_>>()
+                })
                 .ok_or(CatalogErr::InvalidGlassDescription)?;
-            if catalog.insert(
+            if let Some(_) = catalog.insert(
                 name.take().ok_or(CatalogErr::NameNotFound)?,
-                Glass::new(dispform, &cd)?)
-                .is_some() {
-                return Err(CatalogErr::DuplicateGlass)
+                Glass::new(dispform, &cd)?,
+            ) {
+                return Err(CatalogErr::DuplicateGlass);
             }
         }
     }
