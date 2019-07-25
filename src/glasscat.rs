cc80239ae5edd072c4da54d6859004c561690390
Julia Tatz <tatz.j@husky.neu.edu>
2019-07-25 19:40:16 -0400

    Better naming, more comments, more plotting, experimenting w/ rust + py

--- src/glasscat.rs
+++ src/glasscat.rs
@@ -4,7 +4,7 @@ use core::str::FromStr;
 use std::collections::BTreeMap;
 
 #[derive(Debug, Display, Clone, Copy)]
-pub enum CatalogErr {
+pub enum CatalogError {
     NameNotFound,
     GlassTypeNotFound,
     InvalidGlassDescription,
@@ -13,47 +13,48 @@ pub enum CatalogErr {
     DuplicateGlass,
 }
 
-impl std::error::Error for CatalogErr {}
+impl std::error::Error for CatalogError {}
 
-#[repr(C)]
 #[derive(Debug, Clone, Copy)]
 pub enum Glass<N> {
-    Simple([N; 6]),
+    Schott([N; 6]),
     Sellmeier1([N; 6]),
 }
 
 impl<N: RealField> Glass<N> {
-    pub fn new(form: i32, cd: &[N]) -> Result<Self, CatalogErr> {
+    pub fn new(form: i32, cd: &[N]) -> Result<Self, CatalogError> {
         macro_rules! slice_to_arr {
             ($n:literal) => {
                 cd.get(..$n)
                     .and_then(|s| s.try_into().ok())
-                    .ok_or(CatalogErr::InvalidGlassDescription)?
+                    .ok_or(CatalogError::InvalidGlassDescription)?
             };
         }
         Ok(match form {
-            1 => Glass::Simple(slice_to_arr!(6)),
+            1 => Glass::Schott(slice_to_arr!(6)),
             2 => Glass::Sellmeier1(slice_to_arr!(6)),
-            _ => return Err(CatalogErr::UnknownGlassType),
+            _ => return Err(CatalogError::UnknownGlassType),
         })
     }
 
     pub fn calc_n(&self, w: N) -> N {
         // wavelength must be in micrometers
         match self {
-            Glass::Simple(cd) => {
+            Glass::Schott(cd) => {
+                let &[c1, c2, c3, c4, c5, c6] = cd;
                 let w2 = w * w;
                 let w4 = w2 * w2;
                 let w6 = w2 * w4;
                 let w8 = w2 * w6;
-                (cd[0] + cd[1] * w2 + cd[2] / w2 + cd[3] / w4 + cd[4] / w6 + cd[5] / w8).sqrt()
+                (c1 + c2 * w2 + c3 / w2 + c4 / w4 + c5 / w6 + c6 / w8).sqrt()
             }
             Glass::Sellmeier1(cd) => {
+                let &[b1, c1, b2, c2, b3, c3] = cd;
                 let w2 = w * w;
                 (N::one()
-                    + cd[0] * w2 / (w2 - cd[1])
-                    + cd[2] * w2 / (w2 - cd[3])
-                    + cd[4] * w2 / (w2 - cd[5]))
+                    + b1 * w2 / (w2 - c1)
+                    + b2 * w2 / (w2 - c2)
+                    + b3 * w2 / (w2 - c3))
                     .sqrt()
             }
         }
@@ -98,7 +99,7 @@ impl<N: RealField> Glass<N> {
 impl<'a, N: RealField> Into<(i32, &'a [N])> for &'a Glass<N> {
     fn into(self) -> (i32, &'a [N]) {
         match self {
-            Glass::Simple(cd) => (1, cd.as_ref()),
+            Glass::Schott(cd) => (1, cd.as_ref()),
             Glass::Sellmeier1(cd) => (2, cd.as_ref()),
         }
     }
@@ -106,21 +107,22 @@ impl<'a, N: RealField> Into<(i32, &'a [N])> for &'a Glass<N> {
 
 pub fn new_catalog<N: RealField + FromStr>(
     file: &str,
-) -> Result<BTreeMap<String, Glass<N>>, CatalogErr> {
+) -> Result<BTreeMap<String, Glass<N>>, CatalogError> {
     let mut catalog = BTreeMap::new();
     let mut name = None;
     let mut dispform: i32 = -1;
     for line in file.lines() {
         if line.starts_with("NM") {
             let nm: Vec<_> = line.split(' ').collect();
-            if let Some(_) = name.replace(nm.get(1).ok_or(CatalogErr::NameNotFound)?.to_uppercase())
+            if let Some(_) =
+                name.replace(nm.get(1).ok_or(CatalogError::NameNotFound)?.to_uppercase())
             {
-                return Err(CatalogErr::GlassDescriptionNotFound);
+                return Err(CatalogError::GlassDescriptionNotFound);
             }
             dispform = nm
                 .get(2)
                 .and_then(|d| d.parse().ok())
-                .ok_or(CatalogErr::GlassTypeNotFound)?;
+                .ok_or(CatalogError::GlassTypeNotFound)?;
         } else if line.starts_with("CD") {
             let cd = line
                 .get(2..)
@@ -129,12 +131,12 @@ pub fn new_catalog<N: RealField + FromStr>(
                         .filter_map(|s| s.parse().ok())
                         .collect::<Vec<_>>()
                 })
-                .ok_or(CatalogErr::InvalidGlassDescription)?;
+                .ok_or(CatalogError::GlassDescriptionNotFound)?;
             if let Some(_) = catalog.insert(
-                name.take().ok_or(CatalogErr::NameNotFound)?,
+                name.take().ok_or(CatalogError::NameNotFound)?,
                 Glass::new(dispform, &cd)?,
             ) {
-                return Err(CatalogErr::DuplicateGlass);
+                return Err(CatalogError::DuplicateGlass);
             }
         }
     }
