072dbadae692ade8045a8864ede1c90ae59b6d8e
Julia Tatz <tatz.j@northeastern.edu>
2019-07-28 19:09:06 -0400

    Fixed typo, & reduced py ffi code

--- src/pylib.rs
+++ src/pylib.rs
@@ -1,5 +1,5 @@
 use cpython::{
-    exc::TypeError, FromPyObject, ObjectProtocol, PyErr, PyObject, PyResult, Python, ToPyObject,
+    exc, FromPyObject, ObjectProtocol, PyErr, PyObject, PyResult, Python, ToPyObject,
 };
 
 use crate::glasscat::{new_catalog, CatalogError as _CatalogError, Glass};
@@ -82,81 +82,40 @@ where
     obj.getattr(py, k)?.extract(py)
 }
 
-fn get_buffer_attr<'p, I, T>(py: Python, obj: &'p PyObject, k: &'static str) -> PyResult<&'p [T]>
+fn get_buffer_attr<'p, T>(py: Python, obj: &'p PyObject, k: &'static str) -> PyResult<&'p [T]>
 where
-    I: cpython::buffer::Element + for<'a> cpython::FromPyObject<'a>,
-    //      T: zerocopy::FromBytes,
+    T: zerocopy::FromBytes,
 {
     let obj = obj.getattr(py, k)?;
     let buffer = cpython::buffer::PyBuffer::get(py, &obj)?;
-    let ptr = buffer.buf_ptr() as *const T;
     if !buffer.is_c_contiguous() {
-        Err(PyErr::new::<TypeError, _>(
-            py,
-            "buffer must be C contiguous",
-        ))
-    } else if (ptr as usize) % core::mem::align_of::<T>() != 0 {
-        Err(PyErr::new::<TypeError, _>(py, "Invalid buffer alignment"))
-    } else if !I::is_compatible_format(buffer.format()) {
-        Err(PyErr::new::<TypeError, _>(py, "Invalid buffer format"))
-    } else if core::mem::size_of::<I>() != buffer.item_size()
-        || buffer.len_bytes() % core::mem::size_of::<T>() != 0
-    {
-        Err(PyErr::new::<TypeError, _>(py, "Invalid buffer size"))
+        Err(PyErr::new::<exc::BufferError, _>(py, "buffer must be C contiguous"))
     } else {
-        let len = buffer.len_bytes() / std::mem::size_of::<T>();
-        Ok(unsafe { core::slice::from_raw_parts(ptr, len) })
-    }
-}
-
-macro_rules! obj_to_struct {
-    (@field $py:ident $obj:ident => $field:ident) => {
-        get_attr($py, $obj, stringify!($field))?
-    };
-    ($py:ident, $obj:ident => $stype:ident { $( $($fields:ident)+ ),* }) => {
-        $stype::new(
-            $( obj_to_struct!(@field $py $obj => $($fields)*) ),*
-        )
-    };
-}
-
-#[derive(Constructor, Debug, Clone)]
-pub struct OwnedPrism {
-    glasses: Vec<Glass>,
-    angles: Vec<f64>,
-    curvature: f64,
-    height: f64,
-    width: f64,
-}
-
-impl<'p> Into<CompoundPrism<'p>> for &'p OwnedPrism {
-    fn into(self) -> CompoundPrism<'p> {
-        CompoundPrism {
-            glasses: &self.glasses,
-            angles: &self.angles,
-            curvature: self.curvature,
-            height: self.height,
-            width: self.width,
+        let bytes = unsafe { core::slice::from_raw_parts(buffer.buf_ptr() as *const u8, buffer.len_bytes()) };
+        if let Some(layout) = zerocopy::LayoutVerified::new_slice(bytes) {
+            Ok(layout.into_slice())
+        } else {
+            Err(PyErr::new::<exc::BufferError, _>(py, "Invalid buffer"))
         }
     }
 }
 
-impl FromPyObject<'_> for OwnedPrism {
-    fn extract(py: Python, obj: &PyObject) -> PyResult<Self> {
-        Ok(obj_to_struct!(py, obj => OwnedPrism{
-            glasses,
-            angles,
-            curvature,
-            height,
-            width
-        }))
+impl<'p> FromPyObject<'p> for CompoundPrism<'p> {
+    fn extract(py: Python, obj: &'p PyObject) -> PyResult<Self> {
+        Ok(CompoundPrism {
+            glasses: get_attr::<Vec<_>>(py, obj, "glasses")?.into(),
+            angles: get_buffer_attr::<f64>(py, obj, "angles")?.into(),
+            curvature: get_attr(py, obj, "curvature")?,
+            height: get_attr(py, obj, "height")?,
+            width: get_attr(py, obj, "width")?,
+        })
     }
 }
 
 impl<'p> FromPyObject<'p> for DetectorArray<'p> {
     fn extract(py: Python, obj: &'p PyObject) -> PyResult<Self> {
         Ok(DetectorArray {
-            bins: get_buffer_attr::<f64, [f64; 2]>(py, obj, "bins")?,
+            bins: get_buffer_attr::<[f64; 2]>(py, obj, "bins")?.into(),
             min_ci: get_attr(py, obj, "min_ci")?,
             angle: get_attr(py, obj, "angle")?,
             length: get_attr(py, obj, "length")?,
@@ -197,17 +156,17 @@ fn init_mod(py: Python, m: &cpython::PyModule) -> PyResult<()> {
                 new_catalog(file).into_py_result(py)
         }),
     )?;
-    m.add(py, "fitness", py_fn!(py, __fitness(prism: OwnedPrism, pmts: DetectorArray, beam: GaussianBeam) -> PyResult<impl ToPyObject> {
-        py.allow_threads(|| fitness((&prism).into(), pmts, beam).map(|arr| Vec::from(arr.as_ref()))).into_py_result(py)
+    m.add(py, "fitness", py_fn!(py, __fitness(prism: CompoundPrism, pmts: DetectorArray, beam: GaussianBeam) -> PyResult<impl ToPyObject> {
+        py.allow_threads(|| fitness(&prism, &pmts, &beam).map(|arr| Vec::from(arr.as_ref()))).into_py_result(py)
     }))?;
-    m.add(py, "trace", py_fn!(py, __trace(wavelength: f64, init_y: f64, prism: OwnedPrism, pmts: DetectorArray, det: DetectorArrayPositioning) -> PyResult<impl ToPyObject> {
-        py.allow_threads(|| trace(wavelength, init_y, (&prism).into(), pmts, det)).into_py_result(py)
+    m.add(py, "trace", py_fn!(py, __trace(wavelength: f64, init_y: f64, prism: CompoundPrism, pmts: DetectorArray, det: DetectorArrayPositioning) -> PyResult<impl ToPyObject> {
+        py.allow_threads(|| trace(wavelength, init_y, &prism, &pmts, det)).into_py_result(py)
     }))?;
-    m.add(py, "transmission", py_fn!(py, __transmission(wavelengths: Vec<f64>, prism: OwnedPrism, pmts: DetectorArray, beam: GaussianBeam, det: DetectorArrayPositioning) -> PyResult<impl ToPyObject> {
-        py.allow_threads(|| Ok(transmission(&wavelengths, (&prism).into(), pmts, beam, det)))
+    m.add(py, "transmission", py_fn!(py, __transmission(wavelengths: Vec<f64>, prism: CompoundPrism, pmts: DetectorArray, beam: GaussianBeam, det: DetectorArrayPositioning) -> PyResult<impl ToPyObject> {
+        py.allow_threads(|| Ok(transmission(&wavelengths, &prism, &pmts, &beam, det)))
     }))?;
-    m.add(py, "spectrometer_position", py_fn!(py, __spectrometer_position(prism: OwnedPrism, pmts: DetectorArray, beam: GaussianBeam) -> PyResult<impl ToPyObject> {
-        py.allow_threads(|| detector_array_positioning((&prism).into(), pmts, beam)).into_py_result(py)
+    m.add(py, "spectrometer_position", py_fn!(py, __spectrometer_position(prism: CompoundPrism, pmts: DetectorArray, beam: GaussianBeam) -> PyResult<impl ToPyObject> {
+        py.allow_threads(|| detector_array_positioning(&prism, &pmts, &beam)).into_py_result(py)
     }))?;
     Ok(())
 }
