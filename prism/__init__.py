0861953730c6818a7639b82404d4ddef7dfe0952
Julia Tatz <tatz.j@husky.neu.edu>
2019-08-25 00:39:14 -0400

    Prism height is an optimization parameter & max height is a design variable

--- prism/__init__.py
+++ prism/__init__.py
@@ -10,69 +10,70 @@ class Config(typing.NamedTuple):
     max_prism_count: int
     wavelength_range: typing.Tuple[float, float]
     beam_width: float
-    prism_height: float
+    max_prism_height: float
     prism_width: float
     detector_array_length: float
     detector_array_min_ci: float
     detector_array_bin_bounds: np.ndarray
     glasses: typing.Sequence[typing.Tuple[str, PyGlass]]
 
+    @property
+    def params_dtype(self):
+        return np.dtype([
+            ('nprism', 'f8'),
+            ('prism_height', 'f8'),
+            ('glass_indices', 'f8', self.max_prism_count),
+            ('angles', 'f8', self.max_prism_count + 1),
+            ('lengths', 'f8', self.max_prism_count),
+            ('curvature', 'f8'),
+            ('normalized_y_mean', 'f8'),
+            ('detector_array_angle', 'f8'),
+        ])
+
     def params_bounds(self) -> typing.Tuple[typing.Iterable[float], typing.Iterable[float]]:
         nprism = self.max_prism_count
         prism_count_bounds = 1, 1 + self.max_prism_count
+        prism_height_bounds = 0, self.max_prism_height
         glass_bounds = nprism * [(0, len(self.glasses))]
         angle_bounds = (nprism + 1) * [(-np.pi / 2, np.pi / 2)]
-        length_bounds = nprism * [(0., self.prism_height)]
+        length_bounds = nprism * [(0., self.max_prism_height)]
         curvature_bounds = 0.001, 1.0
-        y_mean_bounds = 0, self.prism_height
+        y_mean_bounds = 0, 1
         det_arr_angle_bounds = -np.pi, np.pi
-        return tuple(zip(*[
+        lb, ub = np.transpose([
             prism_count_bounds,
+            prism_height_bounds,
             *glass_bounds,
             *angle_bounds,
             *length_bounds,
             curvature_bounds,
             y_mean_bounds,
             det_arr_angle_bounds,
-        ]))
+        ])
+        return lb, ub
 
-    def array_to_params(self, p: typing.Sequence) -> (CompoundPrism, DetectorArray, GaussianBeam):
-        def slices():
-            arr = np.asarray(p)
-            sizes = 1, self.max_prism_count, self.max_prism_count + 1, self.max_prism_count, 1, 1, 1
-            for i in sizes:
-                if i == 1:
-                    yield arr[0]
-                else:
-                    yield arr[:i]
-                arr = arr[i:]
-        params = list(slices())
-        prism_count = int(np.clip(params[0], 1, self.max_prism_count))
-        glass_names, glasses = zip(*(self.glasses[min(int(i), len(self.glasses) - 1)] for i in params[1][:prism_count]))
-        angles = params[2][:prism_count + 1]
-        lengths = params[3][:prism_count]
-        curvature = params[4]
-        y_mean = params[5]
-        detector_array_angle = params[6]
-
+    def array_to_params(self, p: np.ndarray) -> (CompoundPrism, DetectorArray, GaussianBeam):
+        params = np.asanyarray(p).view(self.params_dtype)[0]
+        prism_count = int(np.clip(params['nprism'], 1, self.max_prism_count))
+        prism_height = params['prism_height']
         prism = CompoundPrism(
-            glasses=glasses,
-            angles=angles,
-            lengths=lengths,
-            curvature=curvature,
-            height=self.prism_height,
+            glasses=[self.glasses[min(int(i), len(self.glasses) - 1)][1] for i in params['glass_indices'][:prism_count]],
+            angles=params['angles'][:prism_count + 1],
+            lengths=params['lengths'][:prism_count],
+            curvature=params['curvature'],
+            height=prism_height,
             width=self.prism_width,
         )
         detarr = DetectorArray(
             bins=self.detector_array_bin_bounds,
             min_ci=self.detector_array_min_ci,
-            angle=detector_array_angle,
+            angle=params['detector_array_angle'],
             length=self.detector_array_length,
         )
         beam = GaussianBeam(
             wavelength_range=self.wavelength_range,
             width=self.beam_width,
-            y_mean=y_mean,
+            y_mean=params['normalized_y_mean'] * prism_height,
         )
         return prism, detarr, beam
 
@@ -148,16 +149,10 @@ class PyGmoPrismProblem:
     def fitness(self, v: np.ndarray):
         prism, detarr, beam = self.config.array_to_params(v)
         val = fitness(prism, detarr, beam)
-        if val.size > 30 * self.config.prism_height or abs(val.info) < 0.1:
+        if val.size > 30 * self.config.max_prism_height or abs(val.info) < 0.1:
             return [np.inf] * nobjective
         else:
             return val.size, -val.info, val.deviation
-
-    def batch_fitness(self, vs: np.ndarray):
-        # from multiprocessing.dummy import Pool
-        return np.concatenate(list(map(
-            lambda v: self.fitness(v),
-            vs.reshape(-1, 3 * self.config.max_prism_count + 5))), axis=None)
 
     def get_bounds(self):
         return self.config.params_bounds()
@@ -177,14 +172,13 @@ def use_pygmo(iter_count, thread_count, pop_size, config: Config):
     prob = pg.problem(PyGmoPrismProblem(config))
     bfe = pg.bfe(pg.member_bfe())
     a = pg.nsga2(gen=iter_count, cr=0.98, m=0.1)
-    a.set_bfe(bfe)
     algo = pg.algorithm(a)
     # w = np.array((1e-3, 1, 1e-4))
     # prob = pg.decompose(prob, weight=w / np.sum(w), z=(0, -np.log2(len(config.detector_array_bin_bounds)), 0), method='tchebycheff')
     # algo = pg.algorithm(pg.mbh(pg.gaco(gen=iter_count)))
     # pop = pg.population(prob=prob, size=pop_size)
     # algo.evolve(pop)
-    archi = pg.archipelago(thread_count, algo=algo, prob=prob, pop_size=pop_size, b=bfe)
+    archi = pg.archipelago(thread_count, algo=algo, prob=prob, pop_size=pop_size)
     archi.evolve()
     archi.wait_check()
     '''
