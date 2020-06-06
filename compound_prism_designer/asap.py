e93d49858889489b0b5dc02ecafde961d0531501
Julia Tatz <tatz.j@husky.neu.edu>
2020-06-05 23:01:24 -0400

    Fixed ASAP exporting

--- compound_prism_designer/asap.py
+++ compound_prism_designer/asap.py
@@ -45,32 +45,22 @@ class Spectrometer(NamedTuple):
 def create_asap_macro(spectrometer: Spectrometer) -> str:
     polys, lens_poly, lens_center, lens_radius = spectrometer.compound_prism.polygons()
     midpts = [(l0 + u0) / 2 for l0, u0, _, _ in polys] + [(lens_poly[0] + lens_poly[1]) / 2]
+    lengths = [np.linalg.norm(u0 - l0) for l0, u0, _, _ in polys] + [np.linalg.norm(lens_poly[1] - lens_poly[0])]
     lens_midpt = (lens_poly[-2] + lens_poly[-1]) / 2
-    glass_names = ["AIR"] + [g.name for g in spectrometer.compound_prism.glasses] + ["AIR"]
-    planes = '\n'.join(define_plane(
-        angle=angle,
-        midpt=midpt,
-        height=spectrometer.compound_prism.height,
-        width=spectrometer.compound_prism.width,
-        g1=g1,
-        g2=g2,
-        coated=spectrometer.compound_prism.ar_coated,
-    ) for (angle, midpt, g1, g2) in zip(spectrometer.compound_prism.angles, midpts, glass_names[:-1], glass_names[1:]))
-    lens = define_plane(
-        angle=spectrometer.compound_prism.angles[-1],
-        midpt=lens_midpt,
-        height=spectrometer.compound_prism.height,
-        width=spectrometer.compound_prism.width,
-        g1=glass_names[-2],
-        g2=glass_names[-1],
-        coated=spectrometer.compound_prism.ar_coated,
-        radius=lens_radius,
-    )
+    lens_len = np.linalg.norm(lens_poly[-1] - lens_poly[-2])
+    glass_names = ["AIR"] + [g.name.replace("-", "_") for g in spectrometer.compound_prism.glasses] + ["AIR"]
+    planes = '\n'.join(f"""\
+SURFACE
+    PLANE X {midpt[0]} RECT {l/2} {spectrometer.compound_prism.width/2}
+OBJECT
+    ROTATE Z {np.rad2deg(angle)}
+    INTERFACE COATING {"AR" if spectrometer.compound_prism.ar_coated else "BARE"} {g1} {g2}
+""" for (angle, midpt, l, g1, g2) in zip(spectrometer.compound_prism.angles, midpts, lengths, glass_names[:-1], glass_names[1:]))
     det_midpt = np.asarray(spectrometer.position) + np.asarray(spectrometer.direction) * spectrometer.detector_array.length / 2
     wi, wf = spectrometer.gaussian_beam.wavelength_range
     wm = (wi + wf) / 2
     media = "\n".join(f"    {g(wi)} {g(wm)} {g(wf)} '{g.name}'" for g in set(spectrometer.compound_prism.glasses))
-    sample = f"""\
+    return f"""\
 SYSTEM NEW
 RESET
 AXIS X
@@ -81,48 +71,46 @@ MEDIA
 
 COATING PROPERTIES
     0 1 'AR'
-    0 0 'ABSORB'
-
-ENT OBJECT
+    0 0 'ASB'
 
 {planes}
 
-{lens}
-
 SURFACE
-    RECTANGLE X {det_midpt[0]} {spectrometer.detector_array.length} {spectrometer.compound_prism.width}
-OBJECT 'DETECTOR'
-    SKEW X {np.rad2deg(spectrometer.detector_array.angle)} Y
-    SHIFT Y {det_midpt[1]}
-    INTERFACE COATING 'ABSORB'
-
-GAUSSIAN X 0 0 1000 {spectrometer.gaussian_beam.width}
-
-WINDOW Y X
-PLOT FACETS 5 5
-"""
-    return sample
-
-
-def define_plane(angle: float, midpt: Tuple[float, float], height: float, width: float, g1: str, g2: str, coated: bool, radius: Optional[float] = None) -> str:
-    s, c = np.sin(angle), np.cos(angle)
-    # R = np.array(((c, -s), (s, c)))
-    # ux, uy = R @ (-1, 0)
-    ux, uy = -c, -s
-    if radius is not None:
-        chord_length = height / c
-        apothem = np.sqrt(radius**2 - (chord_length / 2)**2)
-        fs = f"    SAG X 0 SPHERE {apothem}\n"
-    else:
-        fs = ""
-    return f"""\
-SURFACE
-    PLANE NORMAL {ux},{uy},0 {midpt[0]} {midpt[1]} 0
-    LOCAL 0 1000000 0 {height} {-width/2} {width/2}
+    BICONIC X {lens_midpt[0]} {-lens_radius} 0 0 0 RECT {lens_len/2} {spectrometer.compound_prism.width/2}                                                   
 OBJECT
-{fs}\
-    INTERFACE COATING {"AR" if coated else "BARE"} '{g1}' '{g2}'
+    ROTATE Z {np.rad2deg(spectrometer.compound_prism.angles[-1])}
+    INTERFACE COATING {"AR" if spectrometer.compound_prism.ar_coated else "BARE"} {glass_names[-2]} {glass_names[-1]}
+
+SURFACE
+    PLANE X {det_midpt[0]} RECT {spectrometer.detector_array.length/2} {spectrometer.compound_prism.width/2}
+OBJECT 'DETECTOR'
+    ROTATE Z {np.rad2deg(spectrometer.detector_array.angle)}
+    SHIFT Y {det_midpt[1] - spectrometer.compound_prism.height/2}
+    INTERFACE COATING ASB AIR AIR
+
+WAVELENGTH {wi}
+GAUSSIAN X 0 0 1000 {spectrometer.gaussian_beam.width}
+SHIFT Y {spectrometer.gaussian_beam.y_mean - spectrometer.compound_prism.height / 2}
+
+WAVELENGTH {wm}
+GAUSSIAN X 0 0 1000 {spectrometer.gaussian_beam.width}
+SHIFT Y {spectrometer.gaussian_beam.y_mean - spectrometer.compound_prism.height / 2}
+
+WAVELENGTH {wf}
+GAUSSIAN X 0 0 1000 {spectrometer.gaussian_beam.width}
+SHIFT Y {spectrometer.gaussian_beam.y_mean - spectrometer.compound_prism.height / 2}
+
+RETURN
+
+$IO VECTOR REWIND
+$IO PLOT CANCEL
+$PLOT OFF
+WINDOW Y X
+PLOT FACETS 5 5 OVERLAY
+SELECT ALL
+TRACE PLOT 1000
+$VIEW
+$PLOT NORM
+$IO PLOT
+$IO INPUT CLOSE
 """
-
-
-
