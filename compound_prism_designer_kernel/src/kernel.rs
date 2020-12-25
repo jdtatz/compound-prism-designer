76e5c82aaffe1c8c1365405649c1653b4254d97d
Julia Tatz <tatz.j@northeastern.edu>
2020-12-25 15:49:00 -0500

    Kernel output fixed A change in the cuda api led to the output buffer being zeroed after the kernel wrote to it Now using nvptx-sys Added warp_sync in case of indpendent thread scheduling

--- compound_prism_designer_kernel/src/kernel.rs
+++ compound_prism_designer_kernel/src/kernel.rs
@@ -1,43 +1,21 @@
 #![no_std]
 #![no_main]
 #![cfg(target_arch = "nvptx64")]
-#![feature(abi_ptx, stdsimd, link_llvm_intrinsics)]
+#![feature(abi_ptx, link_llvm_intrinsics)]
 
 use compound_prism_spectrometer::{
     Beam, DetectorArray, Float, GaussianBeam, Pair, Qrng, Spectrometer, Vector, Welford,
 };
-use core::{arch::nvptx::*, panic::PanicInfo, slice::from_raw_parts_mut};
-
-#[panic_handler]
-unsafe fn panic_handle(p: &PanicInfo) -> ! {
-    let (file, line) = if let Some(loc) = p.location() {
-        (loc.file().as_ptr(), loc.line())
-    } else {
-        (b"".as_ptr(), 0)
-    };
-    if let Some(s) = p.payload().downcast_ref::<&str>() {
-        __assert_fail(s.as_ptr(), file, line, b"".as_ptr());
-    } else {
-        __assert_fail(b"Panicked".as_ptr(), file, line, b"".as_ptr());
-    }
-    trap()
-}
+use core::slice::from_raw_parts_mut;
+use nvptx_sys::{
+    blockDim, blockIdx, dynamic_shared_memory, syncthreads, threadIdx, Shuffle, ALL_MEMBER_MASK,
+};
 
 extern "C" {
-    #[link_name = "llvm.nvvm.ptr.shared.to.gen"]
-    fn ptr_shared_to_gen(shared_ptr: *mut core::ffi::c_void) -> *mut core::ffi::c_void;
-
-    #[link_name = "llvm.nvvm.read.ptx.sreg.laneid"]
-    fn read_ptx_sreg_laneid() -> u32;
-
-    #[link_name = "llvm.nvvm.shfl.sync.bfly.f32"]
-    fn shfl_bfly_sync_f32(mask: u32, val: f32, lane_mask: u32, packing: u32) -> f32;
-
-    #[link_name = "llvm.nvvm.shfl.sync.bfly.i32"]
-    fn shfl_bfly_sync_i32(mask: u32, val: i32, lane_mask: u32, packing: u32) -> i32;
-
     #[link_name = "llvm.nvvm.vote.ballot.sync"]
     fn vote_ballot_sync(lane_mask: u32, pred: bool) -> u32;
+    #[link_name = "llvm.nvvm.bar.warp.sync"]
+    fn warp_sync(membermask: u32);
 }
 
 unsafe fn warp_ballot(pred: bool) -> u32 {
@@ -54,24 +32,19 @@ impl<T: Shareable + Float> CudaFloat for T {}
 
 impl Shareable for i32 {
     unsafe fn shfl_bfly_sync(self, lane_mask: u32) -> Self {
-        shfl_bfly_sync_i32(0xFFFFFFFFu32, self, lane_mask, 0x1F)
+        Shuffle::shfl_bfly(self, ALL_MEMBER_MASK, lane_mask)
     }
 }
 
 impl Shareable for u32 {
     unsafe fn shfl_bfly_sync(self, lane_mask: u32) -> Self {
-        core::mem::transmute(shfl_bfly_sync_i32(
-            0xFFFFFFFFu32,
-            core::mem::transmute(self),
-            lane_mask,
-            0x1F,
-        ))
+        core::mem::transmute(core::mem::transmute::<u32, i32>(self).shfl_bfly_sync(lane_mask))
     }
 }
 
 impl Shareable for f32 {
     unsafe fn shfl_bfly_sync(self, lane_mask: u32) -> Self {
-        shfl_bfly_sync_f32(0xFFFFFFFFu32, self, lane_mask, 0x1F)
+        Shuffle::shfl_bfly(self, ALL_MEMBER_MASK, lane_mask)
     }
 }
 
@@ -86,8 +59,8 @@ impl Shareable for f64 {
         "}") : "=d"(out) : "r"(xor),"d"(self));
         out*/
         let [lo, hi]: [i32; 2] = core::mem::transmute(self);
-        let lo = shfl_bfly_sync_i32(0xFFFFFFFFu32, lo, lane_mask, 0x1F);
-        let hi = shfl_bfly_sync_i32(0xFFFFFFFFu32, hi, lane_mask, 0x1F);
+        let lo = lo.shfl_bfly_sync(lane_mask);
+        let hi = hi.shfl_bfly_sync(lane_mask);
         core::mem::transmute([lo, hi])
     }
 }
@@ -114,11 +87,11 @@ unsafe fn cuda_memcpy_1d<T>(dest: *mut u8, src: &T) -> &T {
     let dest = dest as *mut u32;
     let src = src as *const T as *const u32;
     let count = (core::mem::size_of::<T>() / core::mem::size_of::<u32>()) as u32;
-    let mut id = _thread_idx_x() as u32;
+    let mut id = threadIdx::x() as u32;
     while id < count {
         dest.add(id as usize)
             .write_volatile(src.add(id as usize).read_volatile());
-        id += _block_dim_x() as u32;
+        id += blockDim::x() as u32;
     }
     &*(dest as *const T)
 }
@@ -127,7 +100,7 @@ unsafe fn kernel<F: CudaFloat, V: Vector<Scalar = F>, B: Beam<Vector = V>>(
     seed: F,
     max_evals: u32,
     spectrometer: &Spectrometer<V, B>,
-    prob: *mut F,
+    probability_ptr: *mut F,
 ) {
     if max_evals == 0 {
         return;
@@ -137,18 +110,18 @@ unsafe fn kernel<F: CudaFloat, V: Vector<Scalar = F>, B: Beam<Vector = V>>(
     const PHI: f64 = 1.61803398874989484820458683436563;
     const ALPHA: f64 = 1_f64 / PHI;
 
-    let laneid = read_ptx_sreg_laneid();
-    let tid = _thread_idx_x() as u32;
+    let tid = threadIdx::x() as u32;
     let warpid = tid / 32;
-    let nwarps = _block_dim_x() as u32 / 32;
+    let nwarps = blockDim::x() as u32 / 32;
+    let laneid = nvptx_sys::laneid() as u32;
 
-    let ptr: *mut u8 = ptr_shared_to_gen(0 as _) as _;
-    let spec_ptr = ptr;
-    let prob_ptr = spec_ptr.add(core::mem::size_of::<Spectrometer<V, B>>());
-    let ptr = prob_ptr as *mut Welford<F>;
+    let (ptr, _dyn_mem) = dynamic_shared_memory();
+    let spec_ptr = ptr as *mut u8;
+    let ptr = spec_ptr.add(core::mem::size_of::<Spectrometer<V, B>>());
+    let ptr = ptr as *mut Welford<F>;
 
     let spectrometer = cuda_memcpy_1d(spec_ptr, spectrometer);
-    _syncthreads();
+    syncthreads();
     let nbin = spectrometer.detector.bin_count();
     let shared = from_raw_parts_mut(ptr.add((warpid * nbin) as usize), nbin as usize);
     let mut i = laneid;
@@ -156,8 +129,9 @@ unsafe fn kernel<F: CudaFloat, V: Vector<Scalar = F>, B: Beam<Vector = V>>(
         shared[i as usize] = Welford::new();
         i += 32;
     }
+    warp_sync(ALL_MEMBER_MASK);
 
-    let id = (_block_idx_x() as u32) * nwarps + warpid;
+    let id = (blockIdx::x() as u32) * nwarps + warpid;
 
     let u = (F::from_u32(id)).mul_add(F::from_f64(ALPHA), seed).fract();
     let wavelength = spectrometer.beam.inverse_cdf_wavelength(u);
@@ -205,6 +179,7 @@ unsafe fn kernel<F: CudaFloat, V: Vector<Scalar = F>, B: Beam<Vector = V>>(
             if laneid == 0 {
                 shared[min_index as usize] = welford;
             }
+            warp_sync(ALL_MEMBER_MASK);
             finished = finished && welford.sem_le_error_threshold(F::from_f64(MAX_ERR_SQR));
             if bin_index == min_index {
                 bin_index = nbin;
@@ -216,12 +191,13 @@ unsafe fn kernel<F: CudaFloat, V: Vector<Scalar = F>, B: Beam<Vector = V>>(
         index += 32;
         count += F::from_u32(32);
     }
-    let prob = from_raw_parts_mut(prob.add((nbin * id) as usize), nbin as usize);
+    warp_sync(ALL_MEMBER_MASK);
+    let probability = from_raw_parts_mut(probability_ptr.add((nbin * id) as usize), nbin as usize);
     let mut i = laneid;
     while i < nbin {
         let w = &mut shared[i as usize];
         w.skip(count);
-        prob[i as usize] = w.mean;
+        probability[i as usize] = w.mean;
         i += 32;
     }
 }
