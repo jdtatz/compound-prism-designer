jdtatz/compound-prism-designer

commit e47932084499e89015cd59c8d6fa26c4188b45d6

Ran formatter, removed an extra fn in the kernel Fixed ray tracer small change to almost_eq, for infs cmp sign not val

src/fitness.rs
@@ -192,7 +192,7 @@ mod tests {
                 curvature,
                 prism_height,
                 prism_width,
-                false
+                false,
             );
 
             let detarr_angle = rng.gen_range(-PI, PI);
@@ -276,7 +276,7 @@ mod tests {
             0.21,
             2.5,
             2.,
-            false
+            false,
         );
 
         const NBIN: usize = 32;

src/qrng.rs
@@ -136,7 +136,6 @@ impl<F: Float> QuasiRandom for [F; 2] {
     }
 }
 
-
 pub struct Qrng<Q: QuasiRandom> {
     state: Q,
     alpha: Q,

src/cuda_fitness.rs
@@ -1,17 +1,18 @@
 use crate::fitness::DesignFitness;
 use crate::ray::Spectrometer;
 use crate::utils::{Float, Welford};
-use once_cell::sync::{OnceCell, Lazy};
+use once_cell::sync::{Lazy, OnceCell};
 use parking_lot::Mutex;
 use rustacuda::context::ContextStack;
 use rustacuda::memory::{DeviceBox, DeviceBuffer, DeviceCopy};
 use rustacuda::prelude::*;
 use rustacuda::{launch, quick_init};
-use std::ffi::{CString, CStr};
+use std::ffi::{CStr, CString};
 
 unsafe impl<F: Float + DeviceCopy> DeviceCopy for Spectrometer<F> {}
 
-const PTX_STR: &str = include_str!("../target/nvptx64-nvidia-cuda/release/compound_prism_designer.ptx");
+const PTX_STR: &str =
+    include_str!("../target/nvptx64-nvidia-cuda/release/compound_prism_designer.ptx");
 
 // post-processed generated ptx
 static PTX: Lazy<CString> = Lazy::new(|| {

src/geom.rs
@@ -1,4 +1,7 @@
-use crate::{debug_assert_almost_eq, utils::{LossyInto, Float, almost_eq}};
+use crate::{
+    debug_assert_almost_eq,
+    utils::{almost_eq, Float, LossyInto},
+};
 
 /// vector in R^2 represented as a 2-tuple
 #[repr(C)]
@@ -48,7 +51,7 @@ impl<F1: Float + LossyInto<F2>, F2: Float> LossyInto<Pair<F2>> for Pair<F1> {
     fn into(self) -> Pair<F2> {
         Pair {
             x: self.x.into(),
-            y: self.y.into()
+            y: self.y.into(),
         }
     }
 }
@@ -101,7 +104,10 @@ impl<F: Float> core::ops::Mul<Pair<F>> for Mat2<F> {
 pub trait Surface {
     type Point;
     type UnitVector;
-    fn intersection(&self, ray: (Self::Point, Self::UnitVector)) -> Option<(Self::Point, Self::UnitVector)>;
+    fn intersection(
+        &self,
+        ray: (Self::Point, Self::UnitVector),
+    ) -> Option<(Self::Point, Self::UnitVector)>;
 }
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
@@ -111,8 +117,8 @@ pub struct Plane<F: Float> {
     height: F,
 }
 
+#[cfg(not(target_arch = "nvptx64"))]
 impl<F: Float> Plane<F> {
-    #[cfg(not(target_arch = "nvptx64"))]
     pub(crate) fn end_points(&self, height: F) -> (Pair<F>, Pair<F>) {
         let dx = self.normal.y / self.normal.x * height * F::from_f64(0.5);
         let ux = self.midpt.x - dx;
@@ -131,7 +137,10 @@ impl<F: Float> Surface for Plane<F> {
     type Point = Pair<F>;
     type UnitVector = Pair<F>;
 
-    fn intersection(&self, ray: (Self::Point, Self::UnitVector)) -> Option<(Self::Point, Self::UnitVector)> {
+    fn intersection(
+        &self,
+        ray: (Self::Point, Self::UnitVector),
+    ) -> Option<(Self::Point, Self::UnitVector)> {
         let (origin, direction) = ray;
         debug_assert!(direction.is_unit());
         let ci = -direction.dot(self.normal);
@@ -148,18 +157,30 @@ impl<F: Float> Surface for Plane<F> {
     }
 }
 
-pub(crate) fn create_joined_trapezoids<'s, F: Float>(height: F, angles: &'s [F], sep_lengths: &'s [F])
-    -> (Plane<F>, impl 's + ExactSizeIterator<Item=Plane<F>>) {
+pub(crate) fn create_joined_trapezoids<'s, F: Float>(
+    height: F,
+    angles: &'s [F],
+    sep_lengths: &'s [F],
+) -> (Plane<F>, impl 's + ExactSizeIterator<Item = Plane<F>>) {
     debug_assert!(angles.len() >= 2);
     debug_assert!(angles.len() == sep_lengths.len() + 1);
     let h2 = height * F::from_f64(0.5);
     let normal = -Pair::angled(angles[0]);
-    debug_assert_eq!(normal, rotate(angles[0], Pair { x: -F::one(), y: F::zero() }));
+    debug_assert_eq!(
+        normal,
+        rotate(
+            angles[0],
+            Pair {
+                x: -F::one(),
+                y: F::zero()
+            }
+        )
+    );
     debug_assert_almost_eq!(
-            (normal.y / normal.x).abs().to_f64(),
-            angles[0].tan().abs().to_f64(),
-            1e-10
-        );
+        (normal.y / normal.x).abs().to_f64(),
+        angles[0].tan().abs().to_f64(),
+        1e-10
+    );
     let first = Plane {
         height,
         normal,
@@ -171,26 +192,27 @@ pub(crate) fn create_joined_trapezoids<'s, F: Float>(height: F, angles: &'s [F],
     let mut prev_sign = normal.y.is_sign_positive();
     let mut d1 = first.midpt.x;
     let mut mx = first.midpt.x;
-    let rest = angles[1..].iter().copied().zip(sep_lengths.iter().copied())
+    let rest = angles[1..]
+        .iter()
+        .copied()
+        .zip(sep_lengths.iter().copied())
         .map(move |(angle, sep_len)| {
             let normal = -Pair::angled(angle);
             let sign = normal.y.is_sign_positive();
             let d2 = (normal.y / normal.x).abs() * h2;
-            let sep_dist = sep_len + if prev_sign != sign {
-                d1 + d2
-            } else {
-                (d1 - d2).abs()
-            };
+            let sep_dist = sep_len
+                + if prev_sign != sign {
+                    d1 + d2
+                } else {
+                    (d1 - d2).abs()
+                };
             prev_sign = sign;
             d1 = d2;
             mx += sep_dist;
             Plane {
                 height,
                 normal,
-                midpt: Pair {
-                    x: mx,
-                    y: h2,
-                },
+                midpt: Pair { x: mx, y: h2 },
             }
         });
     (first, rest)
@@ -206,8 +228,6 @@ impl<F1: Float + LossyInto<F2>, F2: Float> LossyInto<Plane<F2>> for Plane<F1> {
     }
 }
 
-
-
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub(crate) struct CurvedPlane<F: Float> {
     /// The midpt of the Curved Surface / circular segment
@@ -218,7 +238,7 @@ pub(crate) struct CurvedPlane<F: Float> {
     pub(crate) radius: F,
     /// max_dist_sq = sagitta ^ 2 + (chord_length / 2) ^ 2
     max_dist_sq: F,
-    direction: bool
+    direction: bool,
 }
 
 impl<F: Float> CurvedPlane<F> {
@@ -230,21 +250,31 @@ impl<F: Float> CurvedPlane<F> {
         let apothem = (radius * radius - chord_length * chord_length * F::from_f64(0.25)).sqrt();
         let sagitta = radius - apothem;
         let (center, midpt) = if signed_curvature.is_sign_positive() {
-            (chord.midpt + chord.normal * apothem, chord.midpt - chord.normal * sagitta)
+            (
+                chord.midpt + chord.normal * apothem,
+                chord.midpt - chord.normal * sagitta,
+            )
         } else {
-            (chord.midpt - chord.normal * apothem, chord.midpt + chord.normal * sagitta)
+            (
+                chord.midpt - chord.normal * apothem,
+                chord.midpt + chord.normal * sagitta,
+            )
         };
         Self {
             midpt,
             center,
             radius,
-            max_dist_sq: sagitta * sagitta + chord_length * chord_length * F::from_f64(0.25),
-            direction: signed_curvature.is_sign_positive()
+            max_dist_sq: sagitta * sagitta + chord_length * chord_length * F::from_u32_ratio(1, 4),
+            direction: signed_curvature.is_sign_positive(),
         }
     }
 
     fn is_along_arc(&self, pt: Pair<F>) -> bool {
-        debug_assert_almost_eq!((pt - self.center).norm().to_f64(), self.radius.to_f64(), 1e-6);
+        debug_assert_almost_eq!(
+            (pt - self.center).norm().to_f64(),
+            self.radius.to_f64(),
+            1e-6
+        );
         (pt - self.midpt).norm_squared() <= self.max_dist_sq
     }
 
@@ -284,7 +314,10 @@ impl<F: Float> Surface for CurvedPlane<F> {
     type Point = Pair<F>;
     type UnitVector = Pair<F>;
 
-    fn intersection(&self, ray: (Self::Point, Self::UnitVector)) -> Option<(Self::Point, Self::UnitVector)> {
+    fn intersection(
+        &self,
+        ray: (Self::Point, Self::UnitVector),
+    ) -> Option<(Self::Point, Self::UnitVector)> {
         let (origin, direction) = ray;
         debug_assert!(direction.is_unit());
         let delta = origin - self.center;
@@ -293,7 +326,11 @@ impl<F: Float> Surface for CurvedPlane<F> {
         if discriminant <= F::zero() {
             return None;
         }
-        let d = if self.direction { -ud + discriminant.sqrt() } else { -ud - discriminant.sqrt() };
+        let d = if self.direction {
+            -ud + discriminant.sqrt()
+        } else {
+            -ud - discriminant.sqrt()
+        };
         if d <= F::zero() {
             return None;
         }
@@ -303,7 +340,7 @@ impl<F: Float> Surface for CurvedPlane<F> {
             debug_assert!(snorm.is_unit());
             Some((p, snorm))
         } else {
-           None
+            None
         }
     }
 }
@@ -315,21 +352,17 @@ impl<F1: Float + LossyInto<F2>, F2: Float> LossyInto<CurvedPlane<F2>> for Curved
             center: LossyInto::into(self.center),
             radius: self.radius.into(),
             max_dist_sq: self.max_dist_sq.into(),
-            direction: self.direction
+            direction: self.direction,
         }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::utils::almost_eq;
-    use rand::prelude::*;
     use std::f64::consts::*;
 
     #[test]
-    fn test_many() {
-
-    }
+    fn test_many() {}
 }

src/glasscat.rs
@@ -1,4 +1,4 @@
-use crate::utils::{LossyInto, Float};
+use crate::utils::{Float, LossyInto};
 use arrayvec::ArrayVec;
 use core::str::FromStr;
 
@@ -198,6 +198,24 @@ impl<F: Float> Glass<F> {
             }
         }
     }
+
+    pub fn decompose(&self) -> (&'static str, &[F]) {
+        match self {
+            Glass::Schott(cd) => ("Schott", cd),
+            Glass::Sellmeier1(cd) => ("Sellmeier1", cd),
+            Glass::Sellmeier2(cd) => ("Sellmeier2", cd),
+            Glass::Sellmeier3(cd) => ("Sellmeier3", cd),
+            Glass::Sellmeier4(cd) => ("Sellmeier4", cd),
+            Glass::Sellmeier5(cd) => ("Sellmeier5", cd),
+            Glass::Herzberger(cd) => ("Herzberger", cd),
+            Glass::Conrady(cd) => ("Conrady", cd),
+            Glass::HandbookOfOptics1(cd) => ("HandbookOfOptics1", cd),
+            Glass::HandbookOfOptics2(cd) => ("HandbookOfOptics2", cd),
+            Glass::Extended(cd) => ("Extended", cd),
+            Glass::Extended2(cd) => ("Extended2", cd),
+            Glass::Extended3(cd) => ("Extended3", cd),
+        }
+    }
 }
 
 fn from_iter_to_array<F1: Float + LossyInto<F2>, F2: Float, A: arrayvec::Array<Item = F2>>(
@@ -233,7 +251,6 @@ impl<F1: Float + LossyInto<F2>, F2: Float> LossyInto<Glass<F2>> for Glass<F1> {
         }
     }
 }
-
 
 struct CatalogIter<'s, F: Float + FromStr> {
     file_lines: core::str::Lines<'s>,

src/kernel.rs
@@ -42,7 +42,7 @@ trait Shareable: 'static + Copy + Sized + Send {
     unsafe fn shfl_bfly_sync(self, lane_mask: u32) -> Self;
 }
 
-trait CudaFloat: Shareable + Float { }
+trait CudaFloat: Shareable + Float {}
 
 impl<T: Shareable + Float> CudaFloat for T {}
 
@@ -54,7 +54,12 @@ impl Shareable for i32 {
 
 impl Shareable for u32 {
     unsafe fn shfl_bfly_sync(self, lane_mask: u32) -> Self {
-        core::mem::transmute(shfl_bfly_sync_i32(0xFFFFFFFFu32, core::mem::transmute(self), lane_mask, 0x1F))
+        core::mem::transmute(shfl_bfly_sync_i32(
+            0xFFFFFFFFu32,
+            core::mem::transmute(self),
+            lane_mask,
+            0x1F,
+        ))
     }
 }
 
@@ -81,6 +86,16 @@ impl Shareable for f64 {
     }
 }
 
+impl<F: CudaFloat> Shareable for Welford<F> {
+    unsafe fn shfl_bfly_sync(self, lane_mask: u32) -> Self {
+        Self {
+            count: self.count.shfl_bfly_sync(lane_mask),
+            mean: self.mean.shfl_bfly_sync(lane_mask),
+            m2: self.m2.shfl_bfly_sync(lane_mask),
+        }
+    }
+}
+
 unsafe fn warp_fold<F: Shareable, Op: Fn(F, F) -> F>(mut val: F, fold: Op) -> F {
     for xor in [16, 8, 4, 2, 1].iter().copied() {
         val = fold(val, val.shfl_bfly_sync(xor));
@@ -88,22 +103,10 @@ unsafe fn warp_fold<F: Shareable, Op: Fn(F, F) -> F>(mut val: F, fold: Op) -> F 
     val
 }
 
-unsafe fn share_welford<F: CudaFloat>(welford: &mut Welford<F>) {
-    for xor in [16, 8, 4, 2, 1].iter().copied() {
-        let other = Welford {
-            count: welford.count.shfl_bfly_sync(xor),
-            mean: welford.mean.shfl_bfly_sync(xor),
-            m2: welford.m2.shfl_bfly_sync(xor),
-        };
-        welford.combine(other);
-    }
-}
-
 unsafe fn cuda_memcpy_1d<T>(dest: *mut u8, src: &T) -> &T {
     let dest = dest as *mut u32;
     let src = src as *const T as *const u32;
     let count = (core::mem::size_of::<T>() / core::mem::size_of::<u32>()) as u32;
-    // let count = (core::mem::size_of::<T>()) as u32;
     let mut id = _thread_idx_x() as u32;
     while id < count {
         dest.add(id as usize)
@@ -149,9 +152,7 @@ unsafe fn kernel<F: CudaFloat>(
 
     let id = (_block_idx_x() as u32) * nwarps + warpid;
 
-    let u = (F::from_u32(id))
-        .mul_add(F::from_f64(ALPHA), seed)
-        .fract();
+    let u = (F::from_u32(id)).mul_add(F::from_f64(ALPHA), seed).fract();
     let wavelength = spectrometer.gaussian_beam.inverse_cdf_wavelength(u);
 
     let mut count = F::zero();
@@ -174,8 +175,12 @@ unsafe fn kernel<F: CudaFloat>(
                 core::hint::unreachable_unchecked()
             }
             det_count -= warp_ballot(bin_index.map_or(false, |i| i == min_index));
-            let bin_t = if bin_index.map_or(false, |i| i == min_index) { t } else { F::zero() };
-            let mut welford = if laneid == 0 {
+            let bin_t = if bin_index.map_or(false, |i| i == min_index) {
+                t
+            } else {
+                F::zero()
+            };
+            let welford = if laneid == 0 {
                 let mut w = shared[min_index as usize];
                 w.skip(count);
                 w.next_sample(bin_t);
@@ -187,7 +192,10 @@ unsafe fn kernel<F: CudaFloat>(
                     m2: F::zero(),
                 }
             };
-            share_welford(&mut welford);
+            let welford = warp_fold(welford, |mut w1, w2| {
+                w1.combine(w2);
+                w1
+            });
             if laneid == 0 {
                 shared[min_index as usize] = welford;
             }

src/lib.rs
@@ -10,6 +10,7 @@ extern crate derive_more;
 extern crate serde;
 
 pub mod erf;
+mod geom;
 mod glasscat;
 #[cfg(not(target_arch = "nvptx64"))]
 pub mod optimizer;
@@ -19,7 +20,6 @@ pub mod designer;
 mod pylib;
 pub mod qrng;
 mod ray;
-mod geom;
 #[macro_use]
 pub mod utils;
 #[cfg(all(not(target_arch = "nvptx64"), feature = "cuda"))]

commit d13d79df5d6aa592c86084f9ff1c08152b71a6e5

Updated norminv tests, Makefile, & How to Build Fixed erronously failing test that broke from commit e479320 Before e479320, norminv was broken, I was using 1/sqrt(2) instead of the correct sqrt(2). In e479320 this was fixed but test_with_known_prism wasn't updated

Makefile.toml
@@ -35,11 +35,14 @@ command = "ptxas"
 args = ["-v", "-arch=sm_52", "src/kernel.ptx"]
 
 [tasks.bench]
-dependencies = ["kernel"]
 command = "cargo"
-args = ["bench", "--features=cuda"]
+args = ["bench", "--features=cuda", "-p", "compound_prism_designer"]
 
-[tasks.python]
+[tasks.test]
+command = "cargo"
+args = ["test", "--workspace", "--exclude", "compound_prism_designer_kernel"]
+
+[tasks.build-python]
 command = "maturin"
 args = [
     "build",

benches/benchmark.rs
@@ -16,8 +16,8 @@ impl criterion::profiler::Profiler for GProf {
     }
 
     fn stop_profiling(&mut self, benchmark_id: &str, benchmark_dir: &std::path::Path) {
-        use std::io::Write;
         use pprof::protos::Message;
+        use std::io::Write;
         std::fs::create_dir_all(benchmark_dir).unwrap();
         let profile_path = benchmark_dir.join(format!("{}.pb", benchmark_id));
         if profile_path.exists() {
@@ -114,15 +114,15 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         w_range: (0.5, 0.82),
     };
     let spec = Spectrometer::<Pair<f32>, _>::new(beam, prism, detarr).unwrap();
-    let cpu_fitness = fitness(&spec);
-    let gpu_fitness = cuda_fitness(&spec).unwrap();
+    let cpu_fitness = fitness(&spec, 16_384, 16_384);
+    let gpu_fitness = cuda_fitness(&spec, 256, 2, 16_384).unwrap();
     assert_almost_eq!(cpu_fitness.info as f64, gpu_fitness.info as f64, 1e-2);
 
     c.bench_function("known_design_example", |b| {
-        b.iter(|| fitness(&spec));
+        b.iter(|| fitness(&spec, 16_384, 16_384));
     });
     c.bench_function("cuda_known_design_example", |b| {
-        b.iter(|| cuda_fitness(&spec).unwrap())
+        b.iter(|| cuda_fitness(&spec, 256, 2, 16_384).unwrap())
     });
     println!("cpu: {:?}", cpu_fitness);
     println!("gpu: {:?}", gpu_fitness);

compound_prism_spectrometer/Cargo.toml
@@ -18,6 +18,7 @@ serde_cbor = { version = "0.11", default-features = false }
 rustacuda_core = { git = "https://github.com/jdtatz/RustaCUDA.git", branch = "driver_update" }
 
 [dev-dependencies]
+approx = "0.4"
 statrs = "0.13"
 
 [features]

compound_prism_spectrometer/src/erf.rs
@@ -41,7 +41,7 @@ const SHAW_Q: &[f64] = &[
 
 /// Fast Non-branching Standard Normal inverse CDF
 /// To transform into a normal distribution with stddev=a and mean=b
-/// x = b - a * norminv(u)
+/// x = b +- a * norminv(u)
 /// precision is ~1E-9 when 1E-15 <= u <= 1 - 1E-15
 /// precision is ~1E-6 when 1E-22 <= u <= 1 - 1E-22
 /// precision is ~1E-3 when 1E-30 <= u <= 1 - 1E-30
@@ -53,6 +53,7 @@ const SHAW_Q: &[f64] = &[
 /// Quantile Mechanics II: Changes of Variables in Monte Carlo methods and GPU-Optimized Normal Quantiles
 /// William T. Shaw, Thomas Luu, Nick Brickman
 pub fn norminv<F: Float>(x: F) -> F {
+    // F::from_f64(-core::f64::consts::SQRT_2 * statrs::function::erf::erfc_inv(2.0 * x.to_f64()))
     let u = if x > F::from_u32_ratio(1, 2) {
         F::one() - x
     } else {
@@ -64,30 +65,151 @@ pub fn norminv<F: Float>(x: F) -> F {
     (v * p / q).copy_sign(x - F::from_u32_ratio(1, 2))
 }
 
+const FAST_SHAW_P: &[f64] = &[
+    1.2533141012558299407,
+    2.4101601285733391215,
+    1.3348090307272045436,
+    0.23753954196273241709,
+    0.011900603295838260268,
+    1.1051591117060895699e-4,
+];
+
+const FAST_SHAW_Q: &[f64] = &[
+    1.0,
+    2.4230267574304831865,
+    1.8481138350821456213,
+    0.50950202270351517687,
+    0.046292707412622896113,
+    0.0010579909915338770381,
+    2.5996479253181457637e-6,
+];
+
+/// Fast Non-branching Standard Normal inverse CDF
+/// To transform into a normal distribution with stddev=a and mean=b
+/// x = b +- a * norminv(u)
+/// precision is ~2.98E-8 when 2.22E-10 <= u <= 1 - 2.22E-10
+/// precision is ~3.1E-8 when 1E-11 <= u <= 1 - 1E-11
+/// precision is ~1E-6 when 1E-12 <= u <= 1 - 1E-12
+/// precision is ~1E-4 when 1E-17 <= u <= 1 - 1E-16
+///
+/// Source:
+/// arXiv:0901.0638v5 [q-fin.CP]
+/// Quantile Mechanics II: Changes of Variables in Monte Carlo methods and GPU-Optimized Normal Quantiles
+/// William T. Shaw, Thomas Luu, Nick Brickman
+pub fn fast_norminv<F: Float>(u: F) -> F {
+    let half_minus_u = F::from_u32_ratio(1, 2) - u;
+    let mut x = (u * F::from_u32(2)).copy_sign(half_minus_u);
+    if half_minus_u < F::zero() {
+        x = x + F::from_u32(2);
+    }
+    let v = -F::ln(x);
+    let p = polynomial(v, FAST_SHAW_P);
+    let q = polynomial(v, FAST_SHAW_Q);
+    (-(p / q)) * v.copy_sign(half_minus_u)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::assert_almost_eq;
+    use approx::assert_relative_eq as assert_almost_eq;
     use statrs::distribution::{InverseCDF, Normal};
 
     #[test]
     fn test_shaw() {
         let n = Normal::new(0.0, 1.0).unwrap();
 
-        assert_almost_eq!(norminv(1e-100), n.inverse_cdf(1e-100), 2e-1);
-        assert_almost_eq!(norminv(1e-60), n.inverse_cdf(1e-60), 2e-2);
-        assert_almost_eq!(norminv(1e-30), n.inverse_cdf(1e-30), 1e-3);
-        assert_almost_eq!(norminv(1e-20), n.inverse_cdf(1e-20), 1e-5);
-        assert_almost_eq!(norminv(1e-15), n.inverse_cdf(1e-15), 5e-9);
-        assert_almost_eq!(norminv(1e-10), n.inverse_cdf(1e-10), 5e-9);
-        assert_almost_eq!(norminv(1e-5), n.inverse_cdf(1e-5), 2e-9);
-        assert_almost_eq!(norminv(0.1), n.inverse_cdf(0.1), 1e-9);
-        assert_almost_eq!(norminv(0.2), n.inverse_cdf(0.2), 1e-9);
-        assert_almost_eq!(norminv(0.5), n.inverse_cdf(0.5), 1e-9);
-        assert_almost_eq!(norminv(0.7), n.inverse_cdf(0.7), 1e-9);
-        assert_almost_eq!(norminv(0.9), n.inverse_cdf(0.9), 1e-9);
-        assert_almost_eq!(norminv(0.99), n.inverse_cdf(0.99), 2.04e-9);
-        assert_almost_eq!(norminv(0.999), n.inverse_cdf(0.999), 2.5e-9);
-        assert_almost_eq!(norminv(0.9999), n.inverse_cdf(0.9999), 4e-9);
+        assert_almost_eq!(norminv(1e-100), n.inverse_cdf(1e-100), max_relative = 2e-1);
+        assert_almost_eq!(norminv(1e-60), n.inverse_cdf(1e-60), max_relative = 2e-2);
+        assert_almost_eq!(norminv(1e-30), n.inverse_cdf(1e-30), max_relative = 1e-3);
+        assert_almost_eq!(norminv(1e-20), n.inverse_cdf(1e-20), max_relative = 1e-5);
+        assert_almost_eq!(norminv(1e-15), n.inverse_cdf(1e-15), max_relative = 5e-9);
+        assert_almost_eq!(norminv(1e-10), n.inverse_cdf(1e-10), max_relative = 5e-9);
+        assert_almost_eq!(norminv(1e-5), n.inverse_cdf(1e-5), max_relative = 2e-9);
+        assert_almost_eq!(norminv(0.1), n.inverse_cdf(0.1), max_relative = 1e-9);
+        assert_almost_eq!(norminv(0.2), n.inverse_cdf(0.2), max_relative = 1e-9);
+        assert_almost_eq!(norminv(0.5), n.inverse_cdf(0.5), max_relative = 1e-9);
+        assert_almost_eq!(norminv(0.7), n.inverse_cdf(0.7), max_relative = 1e-9);
+        assert_almost_eq!(norminv(0.9), n.inverse_cdf(0.9), max_relative = 1e-9);
+        assert_almost_eq!(norminv(0.99), n.inverse_cdf(0.99), max_relative = 2.04e-9);
+        assert_almost_eq!(norminv(0.999), n.inverse_cdf(0.999), max_relative = 2.5e-9);
+        assert_almost_eq!(norminv(0.9999), n.inverse_cdf(0.9999), max_relative = 4e-9);
+    }
+
+    #[test]
+    fn test_fast_shaw() {
+        let n = Normal::new(0.0, 1.0).unwrap();
+
+        assert_almost_eq!(
+            fast_norminv(1e-100),
+            n.inverse_cdf(1e-100),
+            max_relative = 1e-1
+        );
+        assert_almost_eq!(
+            fast_norminv(1e-60),
+            n.inverse_cdf(1e-60),
+            max_relative = 1e-2
+        );
+        assert_almost_eq!(
+            fast_norminv(1e-30),
+            n.inverse_cdf(1e-30),
+            max_relative = 1e-3
+        );
+        assert_almost_eq!(
+            fast_norminv(1e-20),
+            n.inverse_cdf(1e-20),
+            max_relative = 1e-4
+        );
+        assert_almost_eq!(
+            fast_norminv(1e-15),
+            n.inverse_cdf(1e-15),
+            max_relative = 5e-5
+        );
+        assert_almost_eq!(
+            fast_norminv(1e-10),
+            n.inverse_cdf(1e-10),
+            max_relative = 5e-5
+        );
+        assert_almost_eq!(fast_norminv(1e-5), n.inverse_cdf(1e-5), max_relative = 2e-8);
+        assert_almost_eq!(fast_norminv(0.1), n.inverse_cdf(0.1), max_relative = 3e-8);
+        assert_almost_eq!(fast_norminv(0.2), n.inverse_cdf(0.2), max_relative = 3e-8);
+        assert_almost_eq!(fast_norminv(0.5), n.inverse_cdf(0.5), max_relative = 3e-8);
+        assert_almost_eq!(fast_norminv(0.7), n.inverse_cdf(0.7), max_relative = 3e-8);
+        assert_almost_eq!(fast_norminv(0.9), n.inverse_cdf(0.9), max_relative = 3e-8);
+        assert_almost_eq!(fast_norminv(0.99), n.inverse_cdf(0.99), max_relative = 3e-8);
+        assert_almost_eq!(
+            fast_norminv(0.999),
+            n.inverse_cdf(0.999),
+            max_relative = 3e-8
+        );
+        assert_almost_eq!(
+            fast_norminv(0.9999),
+            n.inverse_cdf(0.9999),
+            max_relative = 3e-8
+        );
+        assert_almost_eq!(
+            fast_norminv(1.0 - 1e-5),
+            n.inverse_cdf(1.0 - 1e-5),
+            max_relative = 4e-8
+        );
+        assert_almost_eq!(
+            fast_norminv(1.0 - 1e-10),
+            n.inverse_cdf(1.0 - 1e-10),
+            max_relative = 4e-8
+        );
+        assert_almost_eq!(
+            fast_norminv(1.0 - 1e-11),
+            n.inverse_cdf(1.0 - 1e-11),
+            max_relative = 3e-7
+        );
+        assert_almost_eq!(
+            fast_norminv(1.0 - 1e-15),
+            n.inverse_cdf(1.0 - 1e-15),
+            max_relative = 1e-5
+        );
+        assert_almost_eq!(
+            fast_norminv(1.0 - 1e-16),
+            n.inverse_cdf(1.0 - 1e-16),
+            max_relative = 1e-4
+        );
     }
 }

compound_prism_spectrometer/src/qrng.rs
@@ -1,6 +1,6 @@
 use crate::utils::Float;
 /*
-#![feature(const_fn_floating_point_arithmetic, min_const_generics)]
+#![feature(const_fn_floating_point_arithmetic)]
 
 const fn powi(mut x: f64, mut n: usize) -> f64 {
     if n == 0 {
@@ -142,13 +142,13 @@ impl<const N: usize> Qrng64<N> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::assert_almost_eq;
+    use approx::assert_relative_eq;
 
     #[test]
     fn test_phi() {
-        assert_almost_eq!(phi(1), 1.6180339887498948482045868343656381);
-        assert_almost_eq!(phi(2), 1.3247179572447460259609088544780973, 1e-15);
-        assert_almost_eq!(phi(16), 1.042917732301786579722142992093799834021, 1e-15);
+        assert_relative_eq!(phi(1), 1.6180339887498948482045868343656381);
+        assert_relative_eq!(phi(2), 1.3247179572447460259609088544780973, max_relative = 1e-15);
+        assert_relative_eq!(phi(16), 1.042917732301786579722142992093799834021, max_relative = 1e-15);
     }
 }
 */

environment.yml
@@ -1,11 +1,0 @@
-name: compound_prism_designer_venv
-channels:
-  - conda-forge
-dependencies:
-  - python ~= 3.7
-  - numpy ~= 1.17
-  - matplotlib ~= 3.0
-  - PySide2 ~= 5.13
-  - toml ~= 0.10
-  - pip:
-      - maturin ~= 0.7

src/fitness.rs
@@ -313,8 +313,8 @@ mod tests {
             v.size
         );
         assert!(
-            utils::almost_eq(v.info, 1.44, 1e-2),
-            "Mutual information is incorrect. {} ≉  1.44",
+            utils::almost_eq(v.info, 0.7407094438094362, 1e-2),
+            "Mutual information is incorrect. {} ≉  0.74",
             v.info
         );
         assert!(