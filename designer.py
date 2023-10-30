9966ba17ff7897b02a9f126d2991794a346c2b0c
Julia Tatz <tatz.j@northeastern.edu>
2023-10-30 19:53:02 -0400

    WIP update to pymoo 0.6
    all factory methods are gone and mixed var problems were heavily changed

--- designer.py
+++ designer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-# from __future__ import annotations
-import itertools
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from functools import partial, reduce
 from multiprocessing.pool import ThreadPool
 from operator import mul
@@ -9,14 +7,13 @@ from pathlib import Path
 from typing import Callable, Iterator, List, Optional, Sequence, Tuple, Union
 
 import numpy as np
-from pymoo.core.problem import ElementwiseProblem
-from pymoo.factory import (
-    get_algorithm,
-    get_crossover,
-    get_mutation,
-    get_reference_directions,
-    get_sampling,
+from pymoo.core.mixed import (
+    MixedVariableDuplicateElimination,
+    MixedVariableMating,
+    MixedVariableSampling,
 )
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.core.variable import Integer, Real
 from pymoo.optimize import minimize
 from serde import serde
 from serde.toml import from_toml
@@ -90,7 +87,7 @@ class OptimizerConfig:
 @dataclass
 class CompoundPrismSpectrometerProblemConfig:
     spectrometer: CompoundPrismSpectrometerConfig
-    optimizer: OptimizerConfig = OptimizerConfig()
+    optimizer: OptimizerConfig = field(default_factory=OptimizerConfig)
 
     auto_position_detector_acceptance: Optional[float] = None
     nglass: Optional[int] = None
@@ -98,16 +95,12 @@ class CompoundPrismSpectrometerProblemConfig:
 
     def __post_init__(self):
         if self.nglass is None and self.glass_names is None:
-            raise NotImplementedError(
-                "One of `nglass` or `glass-names` must be defined"
-            )
+            raise NotImplementedError("One of `nglass` or `glass-names` must be defined")
 
     @property
     def nglass_or_const_glasses(self) -> Union[int, Sequence[str]]:
         if self.nglass is None and self.glass_names is None:
-            raise NotImplementedError(
-                "One of `nglass` or `glass-names` must be defined"
-            )
+            raise NotImplementedError("One of `nglass` or `glass-names` must be defined")
         elif self.glass_names is not None:
             return self.glass_names
         else:
@@ -137,103 +130,67 @@ class CompoundPrismSpectrometerProblem(ElementwiseProblem):
         nglass_or_const_glasses = config.nglass_or_const_glasses
         self.use_gaussian_beam = config.spectrometer.beam_width is not None
         self.use_fiber_beam = (
-            config.spectrometer.fiber_core_radius is not None
-            and config.spectrometer.numerical_aperture is not None
+            config.spectrometer.fiber_core_radius is not None and config.spectrometer.numerical_aperture is not None
         )
-        self.auto_position_detector_acceptance = (
-            config.auto_position_detector_acceptance
-        )
+        self.auto_position_detector_acceptance = config.auto_position_detector_acceptance
         assert self.use_gaussian_beam or self.use_fiber_beam
-        assert (
-            self.auto_position_detector_acceptance is None
-            or 0 < self.auto_position_detector_acceptance <= 1
-        )
+        assert self.auto_position_detector_acceptance is None or 0 < self.auto_position_detector_acceptance <= 1
 
         if isinstance(nglass_or_const_glasses, int):
             nglass = nglass_or_const_glasses
             self._glasses = None
-            glass_dtype_fields = [
-                ("glass_indices", (np.int64, (nglass,))),
-            ]
-            glass_bounds = {
-                "glass_indices": [catalog_bounds] * nglass,
-            }
+            glass_vars = {f"glass_idx_{i}": Integer(bounds=catalog_bounds) for i in range(nglass)}
         else:
             self._glasses = [self.glass_dict[n] for n in nglass_or_const_glasses]
             if not all(isinstance(g, Glass) for g in self._glasses):
                 raise TypeError(f"{nglass_or_const_glasses} is not a sequence of Glass")
             nglass = len(self._glasses)
-            glass_dtype_fields = []
-            glass_bounds = {}
-        dtype_fields = [
-            *glass_dtype_fields,
-            ("angles", (np.float64, (nglass + 1,))),
-            ("tanh_lengths", (np.float64, (nglass,))),
-            ("curvature", (np.float64, (1 if self.use_gaussian_beam else 4,))),
-            ("height", np.float64),
-            ("normalized_y_mean", np.float64),
-            ("detector_array_angle", np.float64),
-        ]
-        bounds = {
-            **glass_bounds,
-            "angles": [angle_bounds] * (nglass + 1),
-            "tanh_lengths": [tanh_len_bounds] * nglass,
-            "curvature": [curvature_bounds] * (1 if self.use_gaussian_beam else 4),
-            "height": height_bounds,
-            "normalized_y_mean": normalized_y_mean_bounds,
-            "detector_array_angle": det_arr_angle_bounds,
+            glass_vars = {}
+        self.nglass = nglass
+        if self.use_gaussian_beam:
+            curvature_vars = {"curvature": Real(bounds=curvature_bounds)}
+        else:
+            curvature_vars = {f"curvature_{i}": Real(bounds=curvature_bounds) for i in range(4)}
+        all_vars = {
+            **glass_vars,
+            **{f"angle_{i}": Real(bounds=angle_bounds) for i in range(1 + nglass)},
+            **{f"tanh_len_{i}": Real(bounds=tanh_len_bounds) for i in range(nglass)},
+            **curvature_vars,
+            "height": Real(bounds=height_bounds),
+            "normalized_y_mean": Real(bounds=normalized_y_mean_bounds),
+            "detector_array_angle": Real(bounds=det_arr_angle_bounds),
         }
         if self.auto_position_detector_acceptance is None:
-            dtype_fields.append(("position", (np.float64, (2,))))
-            bounds["position"] = position_bounds
-        self._numpy_dtype = np.dtype(dtype_fields)
-        xl, xu = zip(
-            *itertools.chain.from_iterable(
-                map(lambda v: v if isinstance(v, list) else [v], bounds.values())
-            )
-        )
-
-        fix_indicies = np.add.accumulate(
-            [
-                np.prod(self._numpy_dtype[n].shape, dtype=np.int64)
-                for n in self._numpy_dtype.names
-            ]
-        )[:-1]
-        self._fix_params = lambda *params: np.array(
-            tuple(a.squeeze() for a in np.split(params, fix_indicies)),
-            dtype=self._numpy_dtype,
-        )
+            all_vars["position_dv"], all_vars["position_da"] = (Real(bounds=b) for b in position_bounds)
 
         if config.optimizer.parallelize and "parallelization" not in kwargs:
             pool = ThreadPool()
             kwargs["parallelization"] = ("starmap", pool.starmap)
 
+        for k, v in all_vars.items():
+            print("   ", k, "=", type(v), v.bounds)
         super().__init__(
-            n_var=len(xl),
-            n_obj=1
-            + int(config.optimizer.optimize_size)
-            + int(config.optimizer.optimize_deviation),
+            vars=all_vars,
+            n_obj=1 + int(config.optimizer.optimize_size) + int(config.optimizer.optimize_deviation),
             n_constr=1,
-            xl=xl,
-            xu=xu,
             **kwargs,
         )
+        print(self)
 
     def create_spectrometer(self, params: np.ndarray) -> Spectrometer:
-        params = (self._fix_params)(*params)
         if self.use_gaussian_beam:
             curvature = params["curvature"]
         else:
-            ic0, ic1, fc0, fc1 = params["curvature"]
+            ic0, ic1, fc0, fc1 = (params[f"curvature_{i}"] for i in range(4))
             curvature = ((-ic0, ic1), (fc0, fc1))
         if self._glasses is None:
-            glasses = [self.glass_list[i] for i in params["glass_indices"]]
+            glasses = [self.glass_list[params[f"glass_idx_{i}"]] for i in range(self.nglass)]
         else:
             glasses = self._glasses
         compound_prism = CompoundPrism(
             glasses=glasses,
-            angles=params["angles"],
-            lengths=np.arctanh(params["tanh_lengths"])
+            angles=np.array([params[f"angle_{i}"] for i in range(1 + self.nglass)]),
+            lengths=np.arctanh([params[f"tanh_len_{i}"] for i in range(self.nglass)])
             * self.config.spectrometer.max_height
             / 4,
             curvature=curvature,
@@ -242,9 +199,7 @@ class CompoundPrismSpectrometerProblem(ElementwiseProblem):
             ar_coated=self.config.spectrometer.ar_coated,
         )
         # print(compound_prism)
-        wavelengths = UniformWavelengthDistribution(
-            self.config.spectrometer.wavelength_range
-        )
+        wavelengths = UniformWavelengthDistribution(self.config.spectrometer.wavelength_range)
         if self.use_gaussian_beam:
             beam = GaussianBeam(
                 width=self.config.spectrometer.beam_width,
@@ -257,7 +212,7 @@ class CompoundPrismSpectrometerProblem(ElementwiseProblem):
                 center_y=params["height"] * params["normalized_y_mean"],
             )
         if self.auto_position_detector_acceptance is None:
-            dv, da = params["position"]
+            dv, da = params["position_dv"], params["position_da"]
             c, s = np.cos(da), np.sin(da)
             R = np.array(((c, -s), (s, c)))
             exit_pos, exit_dir = compound_prism.final_midpt_and_normal()
@@ -288,11 +243,7 @@ class CompoundPrismSpectrometerProblem(ElementwiseProblem):
         return Spectrometer(compound_prism, detector_array, wavelengths, beam)
 
     def _evaluate(self, x, out, *args, **kwargs):
-        max_size = (
-            (self.config.spectrometer.max_height * 40)
-            if self.config.optimizer.optimize_size
-            else np.inf
-        )
+        max_size = (self.config.spectrometer.max_height * 40) if self.config.optimizer.optimize_size else np.inf
         max_info = np.log2(self.config.spectrometer.bin_count)
         try:
             spectrometer = self.create_spectrometer(x)
@@ -300,9 +251,7 @@ class CompoundPrismSpectrometerProblem(ElementwiseProblem):
             if self.cpu_only:
                 fit = spectrometer.cpu_fitness()
             else:
-                fit = spectrometer.gpu_fitness(
-                    seeds=np.random.rand(1), max_n=128, max_eval=16_384 // 2
-                )
+                fit = spectrometer.gpu_fitness(seeds=np.random.rand(1), max_n=128, max_eval=16_384 // 2)
                 if fit is None:
                     raise RayTraceError()
             fit_info = np.log2(self.config.spectrometer.bin_count) - fit.info
@@ -314,10 +263,7 @@ class CompoundPrismSpectrometerProblem(ElementwiseProblem):
             if self.config.optimizer.optimize_deviation:
                 result.append(fit.deviation)
             out["F"] = result
-            feasable_size = np.logical_and(
-                self.config.spectrometer.max_height / 2 < fit.size, fit.size < max_size
-            )
-            out["feasible"] = feasable_size
+            feasable_size = np.logical_and(self.config.spectrometer.max_height / 2 < fit.size, fit.size < max_size)
             out["G"] = np.where(feasable_size, 0, 1)
         except RayTraceError:
             result = []
@@ -327,7 +273,6 @@ class CompoundPrismSpectrometerProblem(ElementwiseProblem):
             if self.config.optimizer.optimize_deviation:
                 result.append(1)
             out["F"] = result
-            out["feasible"] = False
             out["G"] = 1
 
 
@@ -369,61 +314,41 @@ problem = CompoundPrismSpectrometerProblem(
     spring_config
 )  # , cpu_only=True, parallelization = ('starmap', pool.starmap))
 
-
-from pymoo.operators.mixed_variable_operator import (
-    MixedVariableCrossover,
-    MixedVariableMutation,
-    MixedVariableSampling,
-)
-
-nglass = problem.config.nglass or 0
-mask = [*(["int"] * nglass), *(["real"] * (problem.n_var - nglass))]
-
-sampling = MixedVariableSampling(
-    mask, {"real": get_sampling("real_lhs"), "int": get_sampling("int_random")}
-)
-
-crossover = MixedVariableCrossover(
-    mask,
-    {
-        "real": get_crossover("real_sbx", eta=15, prob=0.9),
-        "int": get_crossover("int_sbx", eta=15, prob=0.9),
-    },
-)
-
-mutation = MixedVariableMutation(
-    mask,
-    {
-        "real": get_mutation("real_pm", prob=None, eta=20),
-        "int": get_mutation("int_pm", prob=None, eta=20),
-    },
-)
-
-algorithm_kwargs = dict(sampling=sampling, crossover=crossover, mutation=mutation)
+sampling = MixedVariableSampling()
+eliminate_duplicates = MixedVariableDuplicateElimination()
+mating = MixedVariableMating(eliminate_duplicates=eliminate_duplicates)
 pop_size = 1000
-ref_dirs = get_reference_directions("energy", problem.n_obj, 1000)
-
-# 'ga', 'brkga', 'de', 'nelder-mead', 'pattern-search', 'cmaes', 'nsga2', 'rnsga2', 'nsga3', 'unsga3', 'rnsga3', 'moead', 'pso'
-method = "age"
 
 
-if method in {"nsga2", "de", "ga", "pso"}:
-    algorithm = get_algorithm(method, pop_size=pop_size, **algorithm_kwargs)
-elif method == "age":
-    from pymoo.algorithms.moo.age import AGEMOEA
+def get_algorithm(algorithm: str, **kwargs):
+    # NOTE: The AGEMOEA algos require numba
+    if algorithm == "age":
+        from pymoo.algorithms.moo.age import AGEMOEA
 
-    algorithm = AGEMOEA(pop_size=pop_size, **algorithm_kwargs)
-elif method == "age2":
-    from pymoo.algorithms.moo.age2 import AGEMOEA2
+        return AGEMOEA(**kwargs)
+    elif algorithm == "age2":
+        from pymoo.algorithms.moo.age2 import AGEMOEA2
 
-    algorithm = AGEMOEA2(pop_size=pop_size, **algorithm_kwargs)
-else:
-    algorithm = get_algorithm(method, ref_dirs=ref_dirs, **algorithm_kwargs)
+        return AGEMOEA2(**kwargs)
+    elif algorithm == "nsga2":
+        from pymoo.algorithms.moo.nsga2 import NSGA2
+
+        return NSGA2(**kwargs)
+    else:
+        raise NotImplementedError
 
 
-result = minimize(
-    problem, algorithm, termination=("n_gen", 200), verbose=True, save_history=True
+# 'age', 'age2', 'nsga2'
+method = "age2"
+
+algorithm = get_algorithm(
+    method,
+    pop_size=pop_size,
+    sampling=sampling,
+    mating=mating,
+    eliminate_duplicates=eliminate_duplicates,
 )
+result = minimize(problem, algorithm, termination=("n_gen", 200), verbose=True, save_history=True)
 
 if problem.n_obj == 1:
     spec = problem.create_spectrometer(result.X)
